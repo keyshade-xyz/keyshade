81c63ca8c89e0dba801167f0216bb4a8b0b79599
rajdip-b <agentR47@gmail.com>
2023-12-26 19:20:47 +0530

    fix: nx run dev:api failing due to DI error

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { Module } from '@nestjs/common'
+import { UserController } from './user.controller'
+import { UserService } from './user.service'
+import { PrismaRepository } from '../prisma/prisma.repository'
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [
+    UserService,
+    {
+      provide: 'PrismaRepository',
+      useClass: PrismaRepository
+    }
+  ]
 })
 export class UserModule {}
