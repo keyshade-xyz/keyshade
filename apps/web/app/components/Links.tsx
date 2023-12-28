0211ed449dc69f94550218549a0a294719671567
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-12-28 17:33:01 +0530

    #11 fix:
    - fixed alts
    - made components for buttons and twitter handles
    - fixed documentation icon not working
    - changed svg names to match usename
    - updated metadata

---
+++ apps/web/app/components/Links.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ['latin'] })
+type LinksProps = {
+    icon: string
+    description: string
+    link: string
+}
+
+
+const Links = ({ icon, description, link }: LinksProps) => {
+    return (
+        <a
+            href={`${link}`}
+            target="_blank"
+        >
+            <div
+                className={`flex place-items-center gap-2 justify-center rounded-full border-[#3a3e41] border-solid border-[1px] bg-gradient-to-b from-[#0d1215]/45 to-[#323638]/45 px-4 py-2 ${inter.className} cursor-pointer transition-all ease-in-out duration-200 hover:scale-105`}
+            >
+                <Image
+                    src={`./${icon}.svg`}
+                    alt={`${icon}`}
+                    height={13}
+                    width={13}
+                />
+                <p className="text-white text-sm">{description}</p>
+            </div>
+        </a>
+    )
+}
+
+export default Links
