333559d60299608ad63a5a2d45fc6db5843b56c4
Lakshay saini <lakshay.saini@finmo.net>
2023-12-29 22:58:35 +0530

    added more test for controller and service

--- apps/api/src/user/fake.repository.ts
+++ apps/api/src/user/fake.repository.ts
@@ -1,5 +1,6 @@
 import { User } from '@prisma/client'
 import { PrismaRepository } from '../prisma/prisma.repository'
+import { users } from '../mock-data/users'
 
 export const fakeRepository: Partial<PrismaRepository> = {
   async excludeFields<T, K extends keyof T>(
@@ -16,5 +17,31 @@ export const fakeRepository: Partial<PrismaRepository> = {
       id,
       ...data
     })
+  },
+
+  async findUsers(
+    page: number,
+    limit: number,
+    sort: string,
+    order: string,
+    search: string
+  ): Promise<User[]> {
+    let user_data = [...users]
+    user_data.sort((a, b) => {
+      const comparison = a[sort].localeCompare(b[sort])
+      return order === 'desc' ? -comparison : comparison
+    })
+
+    if (search.trim() !== '') {
+      user_data = user_data.filter((user) =>
+        user.name.toLowerCase().includes(search.toLowerCase())
+      )
+    }
+
+    const start_idx = (page - 1) * limit
+    const end_idx = start_idx + limit
+    user_data = user_data.slice(start_idx, end_idx)
+
+    return Promise.resolve(user_data)
   }
 }
--- apps/api/src/user/user.controller.spec.ts
+++ apps/api/src/user/user.controller.spec.ts
@@ -3,6 +3,7 @@ import { UserController } from './user.controller'
 import { UserService } from './user.service'
 import { fakeRepository } from './fake.repository'
 import { User } from '@prisma/client'
+import { users } from '../mock-data/users'
 
 describe('UserController', () => {
   let controller: UserController
@@ -96,17 +97,42 @@ describe('UserController', () => {
     })
   })
 
-  describe('should get all users', () => {
+  describe('get all users', () => {
+    const sorted_user_data: Array<User> = users.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    )
+
     it('should return all users with default parameters', async () => {
-      jest.spyOn(service, 'getAllUsers').mockResolvedValue([user])
-      expect(await controller.getAllUsers()).toEqual([user])
+      jest.spyOn(service, 'getAllUsers').mockResolvedValue(users)
+      expect(await controller.getAllUsers()).toEqual(users)
     })
 
-    it('should return all users with custom parameters', async () => {
-      jest.spyOn(service, 'getAllUsers').mockResolvedValue([user])
-      expect(await controller.getAllUsers(1, 10, 'name', 'asc', '')).toEqual([
-        user
-      ])
+    it('should return only 2 users with a limit of 2', async () => {
+      jest.spyOn(service, 'getAllUsers').mockResolvedValue(users.slice(0, 2))
+      expect(await controller.getAllUsers(1, 2)).toEqual(users.slice(0, 2))
+    })
+
+    it('should return users sorted by name in ascending order', async () => {
+      jest.spyOn(service, 'getAllUsers').mockResolvedValue(sorted_user_data)
+      expect(
+        await controller.getAllUsers(undefined, undefined, 'name', 'asc')
+      ).toEqual(sorted_user_data)
+    })
+
+    const filtered_user_data: Array<User> = users.filter(
+      (user) => user.name === 'John Doe'
+    )
+    it('should return users, where name is "John Doe"', async () => {
+      jest.spyOn(service, 'getAllUsers').mockResolvedValue(filtered_user_data)
+      expect(
+        await controller.getAllUsers(
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          'Jon Doe'
+        )
+      ).toEqual(filtered_user_data)
     })
   })
 })
