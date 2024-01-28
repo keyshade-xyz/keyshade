fec7a9f37523e7ab97f1a9b586377f2fc702e7d5
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-01-28 17:40:04 +0530

    chore: Remove unused SDK folders

--- packages/sdk-node/src/lib/sdk-js.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sdkJs } from './sdk-js';
-
-describe('sdkJs', () => {
-  it('should work', () => {
-    expect(sdkJs()).toEqual('sdk-js');
-  });
-});
--- packages/sdk-node/src/lib/sdk-js.ts
+++
@@ -1,3 +0,0 @@
-export function sdkJs(): string {
-  return 'sdk-js';
-}
