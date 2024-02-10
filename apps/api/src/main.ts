8d372e02305b2749f2bc1cb2e326768332902582
Harsh Patel <harshnpatel9@gmail.com>
2024-02-10 09:54:35 +0530

    feat: init sentry

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,8 @@ import chalk from 'chalk'
 import moment from 'moment'
 import { QueryTransformPipe } from './common/query.transform.pipe'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as Sentry from '@sentry/node'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 
 class CustomLogger implements LoggerService {
   log(message: string) {
@@ -43,30 +45,47 @@ class CustomLogger implements LoggerService {
 }
 
 async function bootstrap() {
-  const logger = new CustomLogger()
-  const app = await NestFactory.create(AppModule, {
-    logger
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE) || 1.0,
+    profilesSampleRate: Number(process.env.SENTRY_PROFILES_SAMPLE_RATE) || 1.0,
+    integrations: [new ProfilingIntegration()],
+    debug: process.env.NODE_ENV.startsWith('dev')
   })
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true
-    }),
-    new QueryTransformPipe()
-  )
-  const port = 4200
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('keyshade')
-    .setDescription('The keyshade API description')
-    .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, swaggerConfig)
-  SwaggerModule.setup('docs', app, document)
-  await app.listen(port)
-  logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  await Sentry.startSpan(
+    {
+      op: 'mainSpan',
+      name: 'My Main Span'
+    },
+    async () => {
+      const logger = new CustomLogger()
+      const app = await NestFactory.create(AppModule, {
+        logger
+      })
+      const globalPrefix = 'api'
+      app.setGlobalPrefix(globalPrefix)
+      app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true,
+          transform: true
+        }),
+        new QueryTransformPipe()
+      )
+      app.use(Sentry.Handlers.requestHandler())
+      const port = 4200
+      const swaggerConfig = new DocumentBuilder()
+        .setTitle('keyshade')
+        .setDescription('The keyshade API description')
+        .setVersion('1.0')
+        .build()
+      const document = SwaggerModule.createDocument(app, swaggerConfig)
+      SwaggerModule.setup('docs', app, document)
+      await app.listen(port)
+      logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+      )
+    }
   )
 }
 
