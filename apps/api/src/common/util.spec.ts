55497fd5c2d9f37b44692f5e08f47cbc5abdf362
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2024-02-11 00:22:59 +0530

    test: Add failing tests in util.spec.ts

--- apps/api/src/common/util.spec.ts
+++ apps/api/src/common/util.spec.ts
@@ -28,39 +28,31 @@ describe('util', () => {
 
   it('should fail to encrypt and decrypt a string', () => {
     const keyPair = createKeyPair()
+    const differenetKeyPair = createKeyPair()
     const plainText = 'hello world'
     const encrypted = encrypt(keyPair.publicKey, plainText)
-    const modifiedText = 'modified hello world'
-    const decrypted = decrypt(keyPair.privateKey, encrypted)
-    expect(decrypted).not.toEqual(modifiedText)
+    const decrypted = () => {
+      decrypt(differenetKeyPair.privateKey, encrypted);
+    };
+    expect(decrypted).toThrow()
   })
 
-  const object = {
-    id: '1',
-    name: 'John Doe',
-    email: 'johndoe@keyshade.xyz',
-    profilePictureUrl: 'https://keyshade.xyz/johndoe.jpg',
-    isActive: true,
-    isOnboardingFinished: false,
-    isAdmin: false
-  }
-
   it('should exclude fields', () => {
+    const object = {
+      id: '1',
+      name: 'John Doe',
+      email: 'johndoe@keyshade.xyz',
+      profilePictureUrl: 'https://keyshade.xyz/johndoe.jpg',
+      isActive: true,
+      isOnboardingFinished: false,
+      isAdmin: false
+    }
+
     const excluded = excludeFields(object, 'isActive')
     expect(excluded).not.toHaveProperty('isActive')
     expect(excluded).toEqual({
       ...object,
       isActive: undefined
-    })
-  })
-
-  it ('should fail to exclude fields', () => {    
-    const excluded = excludeFields(object, 'isActive')
-    excluded.isActive = true
-    expect(excluded).toHaveProperty('isActive')
-    expect(excluded).toEqual({
-      ...object,
-      isActive: true
     })
   })
 })
