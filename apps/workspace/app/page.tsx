47595fd1669ff1d12c9798017a2aac2c0af57480
kriptonian <shymal.sawan@gmail.com>
2024-02-26 19:09:43 +0530

    feat: init the workspace

--- apps/workspace/app/page.tsx
+++ apps/workspace/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Index(): React.JSX.Element {
-  return <div>Workspace</div>
+  return <div>Workspace
+    <Button disabled>
+      Hello
+    </Button>
+  </div>
 }
