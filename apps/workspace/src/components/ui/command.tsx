35f5f4d97395bdd962e972293af4aff7a9dfd574
kriptonian1 <shymal.sawan@gmail.com>
2024-04-24 11:47:44 +0530

    feat(workspace): dashboard ui

--- apps/workspace/src/components/ui/command.tsx
+++ apps/workspace/src/components/ui/command.tsx
@@ -41,7 +41,6 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
-   
   <div
     className="flex items-center border-b border-white/10 px-3"
     // cmdk-input-wrapper=""
