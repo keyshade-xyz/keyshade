568b1469b160cbb3d839d85dfe9870d43cb3f03d
Rajan <106652393+rajanmoliya@users.noreply.github.com>
2024-04-29 11:41:52 +0530

    Update create-user.ts (added requires imports)

--- apps/api/src/common/create-user.ts
+++ apps/api/src/common/create-user.ts
@@ -1,3 +1,9 @@
+import { User, Workspace } from '@prisma/client'
+import { PrismaService } from '../prisma/prisma.service'
+import { CreateUserDto } from '../user/dto/create.user/create.user'
+import createWorkspace from './create-workspace'
+import { Logger } from '@nestjs/common'
+
 const createUser = async (
   dto: Partial<CreateUserDto>,
   prisma: PrismaService
