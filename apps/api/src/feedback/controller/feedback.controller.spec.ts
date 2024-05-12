f5bf43191a3c5f84398bbbe8a9c228f49d0b21c2
rayaanoidPrime <rayaan4dev@gmail.com>
2024-05-12 18:27:11 +0530

    feat(Api): Added Feedback Form Controller

---
+++ apps/api/src/feedback/controller/feedback.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { FeedbackController } from './feedback.controller'
+import { FeedbackService } from '../service/feedback.service'
+import { MAIL_SERVICE } from '../../mail/services/interface.service'
+import { MockMailService } from '../../mail/services/mock.service'
+
+describe('FeedbackController', () => {
+  let controller: FeedbackController
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [FeedbackController],
+      providers: [
+        FeedbackService,
+        { provide: MAIL_SERVICE, useValue: MockMailService }
+      ]
+    }).compile()
+
+    controller = module.get<FeedbackController>(FeedbackController)
+  })
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined()
+  })
+})
---
+++ apps/api/src/feedback/feedback.e2e.spec.ts
@@ -0,0 +1,100 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '../app/app.module'
+import { FeedbackService } from '../feedback/service/feedback.service'
+import { MockMailService } from '../mail/services/mock.service'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { MAIL_SERVICE } from '../mail/services/interface.service'
+import { FeedbackModule } from './feedback.module'
+import { MailModule } from '../mail/mail.module'
+import { PrismaService } from '../prisma/prisma.service'
+import { User } from '@prisma/client'
+import cleanUp from '../common/cleanup'
+
+describe('Feedback Controller (E2E)', () => {
+  let app: NestFastifyApplication
+  let feedbackService: FeedbackService
+  let mockMailService: MockMailService
+  let prisma: PrismaService
+  let user: User
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, FeedbackModule, MailModule]
+    })
+      .overrideProvider(MAIL_SERVICE)
+      .useClass(MockMailService)
+      .compile()
+
+    app = moduleRef.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
+    feedbackService = moduleRef.get(FeedbackService)
+    mockMailService = moduleRef.get(MAIL_SERVICE)
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+    await app.getHttpAdapter().getInstance().ready()
+
+    await cleanUp(prisma)
+
+    user = await prisma.user.create({
+      data: {
+        email: 'john@keyshade.xyz',
+        name: 'John',
+        isActive: true,
+        isAdmin: false,
+        isOnboardingFinished: false
+      }
+    })
+  })
+
+  afterAll(async () => {
+    await cleanUp(prisma)
+    await prisma.$disconnect()
+    await app.close()
+  })
+
+  it('should be defined', async () => {
+    expect(app).toBeDefined()
+    expect(feedbackService).toBeDefined()
+    expect(mockMailService).toBeDefined()
+    expect(prisma).toBeDefined()
+  })
+
+  it('should register feedback successfully', async () => {
+    const feedbackMessage = 'Test feedback message'
+
+    const { statusCode } = await app.inject({
+      method: 'POST',
+      url: '/feedback',
+      payload: { feedback: feedbackMessage },
+      headers: {
+        'x-e2e-user-email': user.email
+      }
+    })
+
+    expect(statusCode).toBe(201)
+  })
+
+  it('should handle empty feedback', async () => {
+    const { statusCode, payload } = await app.inject({
+      method: 'POST',
+      url: '/feedback',
+      payload: { feedback: '' },
+      headers: {
+        'x-e2e-user-email': user.email
+      }
+    })
+
+    expect(statusCode).toBe(400)
+    expect(JSON.parse(payload)).toEqual({
+      error: 'Bad Request',
+      message: 'Feedback cannot be null',
+      statusCode: 400
+    })
+  })
+})
