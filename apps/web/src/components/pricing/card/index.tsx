2c7f1d6171ac0563a13279676ddaf3d098855fee
Harsh Patel <harshnpatel9@gmail.com>
2024-06-27 17:17:52 +0530

    feat(web): Add Pricing Page (#243)
    Co-authored-by: Sawan Bhattacharya <74916308+kriptonian1@users.noreply.github.com>
    Co-authored-by: Rajdip Bhattacharya <agentR47@gmail.com>

---
+++ apps/web/src/components/pricing/card/index.tsx
@@ -0,0 +1,178 @@
+import Image from 'next/image'
+import gridImage from '@public/pricing/grid.png'
+import {
+  ProjectSVG,
+  UserSVG,
+  SupportSVG,
+  TickSVG,
+  StarsLeftSVG,
+  StarsRightSVG
+} from '@public/pricing'
+import type { PriceCardPropsType } from '@/types'
+
+function PriceCard({
+  title,
+  description,
+  price,
+  yearlyPrice,
+  isPopular,
+  spaceProjects,
+  spaceUsers,
+  spaceAccessSpecifier,
+  spaceIntegerations,
+  spaceSecrets,
+  spaceEnvironment,
+  spaceLiveSupport,
+  miscFeatures,
+  PricingType
+}: Readonly<PriceCardPropsType>): React.JSX.Element {
+  const returnButtonLabel = () => {
+    if (price === 0) {
+      return 'Get Started'
+    }
+    if (price < 0) {
+      return 'Contact Us'
+    }
+    return 'Buy Now'
+  }
+
+  return (
+    <div className="relative mt-5 md:mt-0">
+      {isPopular ? (
+        <div className="mt-6 md:mt-0">
+          <div className="absolute -mt-[2.2rem] w-full ">
+            <div className="item h-[4.5rem] translate-y-[2px] overflow-hidden rounded-xl bg-[#1E3F51] bg-clip-border text-center align-middle">
+              <div className="bg h-full w-full bg-gradient-to-b from-[#96D4F8]/5 via-[#96D4F8]/30 to-[#96D4F8]/50">
+                <div className="flex items-center justify-evenly pt-1">
+                  <StarsLeftSVG className="aspect-video w-10" />
+                  <div className="bg-gradient-to-b from-white to-white/70 bg-clip-text text-xs uppercase text-transparent">
+                    Most Popular Plan
+                  </div>
+                  <StarsRightSVG className="aspect-video w-10" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+
+      <div
+        className="border-1 border-brandBlue/30 hover:border-brandBlue/50 relative flex w-[20rem] flex-shrink-0 flex-col rounded-2xl border border-opacity-5 p-5 text-start md:w-[19rem] lg:w-[15rem] xl:w-[18rem]"
+        key={title}
+        style={{
+          background:
+            'linear-gradient(180deg, rgba(50, 60, 80, 0.00) 0%, #252D3F 100%), #313e58'
+        }}
+      >
+        <Image
+          alt="grid image"
+          className=" absolute z-10 -ml-[2rem] -mt-[.5rem] h-[108.39px] w-full"
+          src={gridImage}
+        />
+
+        <div className="text-brandBlue/90 text-xl font-bold sm:text-2xl md:w-auto md:text-3xl">
+          {title}
+        </div>
+
+        <div className="text-brandBlue/80 mt-1 text-sm sm:mt-2 md:text-base">
+          {description}
+        </div>
+
+        {price === 0 ? (
+          <div className="mt-2 text-xl text-white/80 sm:mt-4 sm:text-3xl">
+            Free
+          </div>
+        ) : (
+          <div className="mt-2 flex flex-row items-end justify-start gap-1  text-sm sm:mt-4">
+            <div className="text-xl text-white/80 md:text-3xl">
+              {price < 0
+                ? 'Custom Pricing'
+                : `$${PricingType === 'monthly' ? price : yearlyPrice}`}
+            </div>
+            {price > 0 ? (
+              <div className="text-brandBlue/80 mb-1 text-sm font-light sm:font-normal">
+                {PricingType === 'monthly' ? '/ month' : '/ year'}
+              </div>
+            ) : null}
+          </div>
+        )}
+
+        <button
+          className="border-1 border-brandBlue/80 hover:border-brandBlue/90 bg-brandBlue/30 mb-2 mt-3 h-8 w-28 rounded-full text-white/60 hover:text-white/70 md:mt-4 md:w-32"
+          type="button"
+        >
+          {returnButtonLabel()}
+        </button>
+
+        <div className="my-5 md:-mx-6 lg:-mx-4">
+          <div className="h-[2px] w-full bg-gradient-to-r from-transparent via-[#8EE8FF] to-transparent" />
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <div className="text-brandBlue/80 mt-3 flex flex-row gap-2 text-sm">
+            <ProjectSVG />
+            <div>
+              {spaceProjects < 0 ? 'Unlimited' : spaceProjects} Projects
+            </div>
+          </div>
+          <div className="text-brandBlue/80 mt-1 flex flex-row gap-2 text-sm">
+            <UserSVG />
+            <div>{spaceUsers < 0 ? 'Unlimited' : spaceUsers} Users</div>
+          </div>
+          <div className="text-brandBlue/80 mt-3 flex flex-row gap-2 text-sm">
+            <UserSVG />
+            <div>
+              {spaceEnvironment < 0 ? 'Unlimited' : spaceEnvironment}{' '}
+              Environments
+            </div>
+          </div>
+          <div className="text-brandBlue/80 mt-3 flex flex-row gap-2 text-sm">
+            <UserSVG />
+            <div>{spaceSecrets < 0 ? 'Unlimited' : spaceSecrets} Secrets</div>
+          </div>
+          <div className="text-brandBlue/80 mt-3 flex flex-row gap-2 text-sm">
+            <UserSVG />
+            <div>
+              {spaceIntegerations < 0 ? 'Unlimited' : spaceIntegerations}{' '}
+              Integerations
+            </div>
+          </div>
+
+          <div className="text-brandBlue/80 mt-3 flex flex-row gap-2 text-sm">
+            <UserSVG />
+            <div>{spaceAccessSpecifier} Of Access Specifier </div>
+          </div>
+
+          <div className="text-brandBlue/80 mt-1 flex flex-row gap-2 text-sm">
+            <SupportSVG />
+            {spaceLiveSupport ? (
+              <div> Email & Live Support</div>
+            ) : (
+              <div> Email Support</div>
+            )}
+          </div>
+        </div>
+
+        <div className="text-brandBlue/90 mt-5 text-sm font-light uppercase tracking-widest md:text-base">
+          Features
+        </div>
+
+        <div className="mt-2 flex flex-col space-y-2">
+          {miscFeatures.map((feature) => (
+            <div
+              className="text-brandBlue/80 mt-1 flex flex-row gap-2 text-sm"
+              key={title + feature}
+            >
+              <div>
+                <TickSVG />
+              </div>
+              <div>{feature}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PriceCard
---
+++ apps/web/src/constants/pricing.ts
@@ -0,0 +1,113 @@
+import type { PriceCardDataType, PriceTabDataType } from '@/types'
+
+export const tabsData: PriceTabDataType = [
+  { id: 'monthly', label: 'Monthly' },
+  { id: 'yearly', label: 'Yearly', tag: '-20%', special: true }
+]
+
+export const PriceCardsData: PriceCardDataType = [
+  {
+    title: 'Free',
+    description:
+      'For hobbyist developers looking to showcase their side projects.',
+    price: 0,
+    isPopular: false,
+    spaceWorkspace: 2,
+    spaceProjects: 3,
+    spaceSecrets: 30,
+    spaceEnvironment: 5,
+    spaceIntegerations: 5,
+    spaceAccessSpecifier: 'Only Public or Private',
+    spaceUsers: 3,
+    miscFeatures: [
+      'Forking Allowed',
+      'Event Monitoring',
+      'Email Alerts',
+      'Access Based Control',
+      '5 Days Log Retention',
+      'No IP Blacklisting',
+      'No IP Whitelisting',
+      'No Secret Auditing',
+      'No Domain Mapping Support'
+    ],
+    spaceLiveSupport: false
+  },
+  {
+    title: 'Personal',
+    description:
+      'For Power Users Shipping their ideas to become future products.',
+    price: 9.99,
+    yearlyPrice: 95.99,
+    isPopular: true,
+    spaceWorkspace: 5,
+    spaceProjects: 5,
+    spaceSecrets: 50,
+    spaceEnvironment: 10,
+    spaceIntegerations: 10,
+    spaceAccessSpecifier: 'All Types',
+    spaceUsers: -1, // -1 => Unlimited
+    miscFeatures: [
+      'Forking Allowed',
+      'Event Monitoring',
+      'Email Alerts',
+      'Access Based Control',
+      '30 Days Log Retention',
+      'No IP Blacklisting',
+      'No IP Whitelisting',
+      'No Secret Auditing',
+      'No Domain Mapping Support'
+    ],
+    spaceLiveSupport: false
+  },
+  {
+    title: 'Team',
+    description: 'For professional teams shipping to production.',
+    price: 19.99,
+    yearlyPrice: 215.99,
+    isPopular: false,
+    spaceWorkspace: 10,
+    spaceProjects: -1,
+    spaceSecrets: -1,
+    spaceEnvironment: -1,
+    spaceIntegerations: 20,
+    spaceAccessSpecifier: 'All Types',
+    spaceUsers: -1, // -1 => Unlimited
+    miscFeatures: [
+      'Forking Allowed',
+      'Event Monitoring',
+      'Email Alerts',
+      'Access Based Control',
+      '30 Days Log Retention',
+      'Full IP Blacklisting',
+      'Full IP Whitelisting',
+      'Full Secret Auditing',
+      'No Domain Mapping Support'
+    ],
+    spaceLiveSupport: false
+  },
+  {
+    title: 'Enterprise',
+    description: 'For large organizations with complex requirements.',
+    price: -1, // -1 => Custom Pricing
+    isPopular: false,
+    spaceWorkspace: -1,
+    spaceProjects: -1,
+    spaceSecrets: -1,
+    spaceEnvironment: -1,
+    spaceIntegerations: -1,
+    spaceAccessSpecifier: 'All Types',
+    spaceUsers: -1, // -1 => Unlimited
+    miscFeatures: [
+      'Forking Allowed',
+      'Event Monitoring',
+      'Email Alerts',
+      'Access Based Control',
+      'Unlimited Log Retention',
+      'IP Blacklisting',
+      'IP Whitelisting',
+      'Secret Auditing',
+      'Domain Mapping Support'
+    ],
+    spaceLiveSupport: false
+  }
+]
