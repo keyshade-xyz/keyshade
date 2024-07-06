9576715f31a6fb60b147125cbf23d1df65f1b58a
kriptonian1 <shymal.sawan@gmail.com>
2024-07-06 18:08:04 +0530

    no clue why it is showing added by me but okey

---
+++ apps/web/src/types/pricing.ts
@@ -0,0 +1,32 @@
+export interface PriceCardPropsType {
+  title: string
+  description: string
+  price: number
+  yearlyPrice?: number
+
+  isPopular: boolean
+
+  spaceWorkspace: number
+  spaceProjects: number
+  spaceEnvironment: number
+  spaceSecrets: number
+  spaceIntegerations: number
+  spaceLiveSupport: boolean
+
+  spaceAccessSpecifier: string
+
+  spaceUsers: number
+
+  miscFeatures: string[]
+  PricingType?: string
+}
+
+export interface PriceTabPropsType {
+  id: string
+  label: string
+  tag?: string
+  special?: boolean
+}
+
+export type PriceTabDataType = PriceTabPropsType[]
+export type PriceCardDataType = PriceCardPropsType[]
