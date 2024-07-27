9dc9f4176a00ff0dce05c4cfb98d6ef16eaef842
vr-varad <varadgupta21#gmail.com>
2024-07-27 10:18:14 +0530

    Initialized the solution with structure

---
+++ packages/api-client/src/controllers/project/project.ts
@@ -0,0 +1,15 @@
+import client from '@package/client'
+
+export default class ProjectController {
+  private static apiClient = client
+
+  static async createProject() {}
+  static async updateProject() {}
+  static async deleteProject() {}
+  static async getProject() {}
+  static async forkProject() {}
+  static async syncFork() {}
+  static async unlinkFork() {}
+  static async getForks() {}
+  static async getAllProjects() {}
+}
