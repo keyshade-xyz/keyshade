8cc5a5cddd4d36cf23e5a5fb32a58909b2811183
vr-varad <varadgupta21#gmail.com>
2024-07-27 19:44:58 +0530

    Resolved Errors and Tests

--- packages/api-client/tests/project.spec.ts
+++ packages/api-client/tests/project.spec.ts
@@ -7,174 +7,129 @@ describe('Get Project Tests', () => {
   let workspaceId: string | null
 
   beforeAll(async () => {
-    try {
-      //Create the user's workspace
-      const workspaceResponse = (await client.post(
-        '/api/workspace',
-        {
-          name: 'My Workspace'
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )) as any
+    //Create the user's workspace
+    const workspaceResponse = (await client.post(
+      '/api/workspace',
+      {
+        name: 'My Workspace'
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )) as any
 
-      workspaceId = workspaceResponse.id
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    workspaceId = workspaceResponse.id
   })
 
   afterAll(async () => {
-    try {
-      // Delete the workspace
-      await client.delete(`/api/workspace/${workspaceId}`, {
-        'x-e2e-user-email': email
-      })
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    // Delete the workspace
+    await client.delete(`/api/workspace/${workspaceId}`, {
+      'x-e2e-user-email': email
+    })
   })
   it('should create a project', async () => {
-    try {
-      const project = await ProjectController.createProject(
-        {
-          name: 'Project',
-          description: 'Project Description',
-          storePrivateKey: true,
-          workspaceId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(project.id).toBeDefined()
-      expect(project.name).toBe('Project')
-      expect(project.description).toBe('Project description')
-      expect(project.storePrivateKey).toBe(true)
-      expect(project.workspaceId).toBe(workspaceId)
-      expect(project.publicKey).toBeDefined()
-      expect(project.privateKey).toBeDefined()
-      expect(project.createdAt).toBeDefined()
-      expect(project.updatedAt).toBeDefined()
-      projectId = project.id
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    const project = await ProjectController.createProject(
+      {
+        name: 'Project',
+        description: 'Project Description',
+        storePrivateKey: true,
+        workspaceId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(project.id).toBeDefined()
+    expect(project.name).toBe('Project')
+    expect(project.description).toBe('Project description')
+    expect(project.storePrivateKey).toBe(true)
+    expect(project.workspaceId).toBe(workspaceId)
+    expect(project.publicKey).toBeDefined()
+    expect(project.privateKey).toBeDefined()
+    expect(project.createdAt).toBeDefined()
+    expect(project.updatedAt).toBeDefined()
+    projectId = project.id
   })
 
   it('should return the project', async () => {
-    try {
-      const project = await ProjectController.getProject(
-        {
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(project.id).toBe(projectId)
-      expect(project.name).toBe('Project')
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    const project = await ProjectController.getProject(
+      {
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(project.id).toBe(projectId)
+    expect(project.name).toBe('Project')
   })
 
   it('should fork the project', async () => {
-    try {
-      const project = await ProjectController.forkProject(
-        {
-          name: 'Project',
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(project.isForked).toBe(true)
-      expect(project.forkedFromId).toBe(projectId)
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    const project = await ProjectController.forkProject(
+      {
+        name: 'Project',
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(project.isForked).toBe(true)
+    expect(project.forkedFromId).toBe(projectId)
   })
 
   it('should get all fork the project', async () => {
-    try {
-      const forks = await ProjectController.getForks(
-        {
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(forks).toHaveLength(1)
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    const forks = await ProjectController.getForks(
+      {
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(forks).toHaveLength(1)
   })
 
   it('should unlink fork the project', async () => {
-    try {
-      await ProjectController.unlinkFork(
-        {
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      const forks = await ProjectController.getForks(
-        {
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(forks).toHaveLength(0)
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    await ProjectController.unlinkFork(
+      {
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    const forks = await ProjectController.getForks(
+      {
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(forks).toHaveLength(0)
   })
 
   it('should get all projects in the workspace', async () => {
-    try {
-      const projects = await ProjectController.getAllProjects(
-        {
-          workspaceId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-      expect(projects).toHaveLength(1)
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    const projects = await ProjectController.getAllProjects(
+      {
+        workspaceId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
+    expect(projects).toHaveLength(1)
   })
 
   it('should get delete a the project', async () => {
-    try {
-      await ProjectController.deleteProject(
-        {
-          projectId
-        },
-        {
-          'x-e2e-user-email': email
-        }
-      )
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+    await ProjectController.deleteProject(
+      {
+        projectId
+      },
+      {
+        'x-e2e-user-email': email
+      }
+    )
   })
 })
