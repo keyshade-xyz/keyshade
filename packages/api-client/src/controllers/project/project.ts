0162a109775e17e7639b0a51d5866da323f3c7f9
vr-varad <varadgupta21#gmail.com>
2024-07-27 11:24:48 +0530

    Added Major Controllers and their types

--- packages/api-client/src/controllers/project/project.ts
+++ packages/api-client/src/controllers/project/project.ts
@@ -1,15 +1,89 @@
 import client from '@package/client'
+import {
+  CreateProjectRequest,
+  DeleteProjectRequest,
+  ForkProjectRequest,
+  GetAllProjectsRequest,
+  GetForkRequest,
+  GetProjectRequest,
+  UnlinkProjectRequest,
+  UpdateProjectRequest
+} from '@package/types/project.types'
 
 export default class ProjectController {
   private static apiClient = client
 
-  static async createProject() {}
-  static async updateProject() {}
-  static async deleteProject() {}
-  static async getProject() {}
-  static async forkProject() {}
+  static async createProject(
+    request: CreateProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.post(
+      `/api/project/${request.workspaceId}`,
+      request,
+      headers
+    )
+  }
+
+  static async updateProject(
+    request: UpdateProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.put(
+      `/api/project/${request.projectId}`,
+      request,
+      headers
+    )
+  }
+
+  static async deleteProject(
+    request: DeleteProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.delete(`/api/project/${request.projectId}`, headers)
+  }
+
+  static async getProject(
+    request: GetProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.get(`/api/project/${request.projectId}`, headers)
+  }
+
+  static async forkProject(
+    request: ForkProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.post(
+      `/api/project/${request.projectId}/fork`,
+      request,
+      headers
+    )
+  }
+
   static async syncFork() {}
-  static async unlinkFork() {}
-  static async getForks() {}
-  static async getAllProjects() {}
+
+  static async unlinkFork(
+    request: UnlinkProjectRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.put(
+      `/api/project/${request.projectId}/unlink-fork`,
+      request,
+      headers
+    )
+  }
+
+  static async getForks(request: GetForkRequest, headers: any): Promise<any> {
+    return this.apiClient.get(
+      `/api/project/${request.projectId}/forks`,
+      headers
+    )
+  }
+
+  static async getAllProjects(
+    request: GetAllProjectsRequest,
+    headers: any
+  ): Promise<any> {
+    return this.apiClient.get(`/api/project/all/${request.projectId}`, headers)
+  }
 }
--- packages/api-client/src/types/project.types.d.ts
+++ packages/api-client/src/types/project.types.d.ts
@@ -1,18 +1,56 @@
-export interface CreateProjectRequest {}
+export interface CreateProjectRequest {
+  name: string
+  workspaceId: string
+  description?: string
+  storePrivateKey?: boolean
+  environments?: CreateEnvironment[]
+}
+
 export interface CreateProjectResponse {}
-export interface UpdateProjectRequest {}
+
+export interface UpdateProjectRequest {
+  projectId: string
+  name?: string
+}
+
 export interface UpdateProjectResponse {}
-export interface DeleteProjectRequest {}
+
+export interface DeleteProjectRequest {
+  projectId: string
+}
+
 export interface DeleteProjectResponse {}
-export interface GetProjectRequest {}
+
+export interface GetProjectRequest {
+  projectId: string
+}
+
 export interface GetProjectResponse {}
-export interface ForkProjectRequest {}
+
+export interface ForkProjectRequest {
+  projectId: string
+  name: string
+}
+
 export interface ForkProjectResponse {}
+
 export interface SyncProjectRequest {}
+
 export interface SyncProjectResponse {}
-export interface UnlinkProjectRequest {}
+
+export interface UnlinkProjectRequest {
+  projectId: string
+}
+
 export interface UnlinkProjectResponse {}
-export interface GetForkRequest {}
+
+export interface GetForkRequest {
+  projectId: string
+}
+
 export interface GetForkResponse {}
-export interface GetAllProjectsRequest {}
+export interface GetAllProjectsRequest {
+  projectId: string
+}
+
 export interface GetAllProjectsResponse {}
