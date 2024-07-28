2986d176fed33925de33480f4096c316e0dbc41b
vr-varad <varadgupta21#gmail.com>
2024-07-28 11:26:13 +0530

    Added Controllers and Types of request and response

--- packages/api-client/src/controllers/secret/secret.ts
+++ packages/api-client/src/controllers/secret/secret.ts
@@ -1,12 +1,78 @@
-import client from '../../client'
+import client from '@package/client'
+import {
+  CreateSecretRequest,
+  CreateSecretResponse,
+  DeleteSecretRequest,
+  DeleteSecretResponse,
+  getAllSecretsOfEnvironmentRequest,
+  getAllSecretsOfEnvironmentResponse,
+  getAllSecretsOfProjectRequest,
+  getAllSecretsOfProjectResponse,
+  RollBackSecretRequest,
+  RollBackSecretResponse,
+  UpdateSecretRequest,
+  UpdateSecretResponse
+} from '@package/types/secret.types'
 
 export default class SecretController {
   private static apiClient = client
 
-  static async createSecret() {}
-  static async updateSecret() {}
-  static async rollbackSecret() {}
-  static async deleteSecret() {}
-  static async getAllSecretsOfProject() {}
-  static async getAllSecretsOfEnvironment() {}
+  static async createSecret(
+    request: CreateSecretRequest,
+    headers?: Record<string, string>
+  ): Promise<CreateSecretResponse> {
+    return this.apiClient.post(
+      `/api/secret/${request.projectId}`,
+      request,
+      headers
+    )
+  }
+  static async updateSecret(
+    request: UpdateSecretRequest,
+    headers?: Record<string, string>
+  ): Promise<UpdateSecretResponse> {
+    return this.apiClient.put(
+      `/api/secret/${request.secretId}`,
+      request,
+      headers
+    )
+  }
+
+  static async rollbackSecret(
+    request: RollBackSecretRequest,
+    headers?: Record<string, string>
+  ): Promise<RollBackSecretResponse> {
+    return this.apiClient.put(
+      `/api/secret/${request.secretId}/rollback/${request.version}?environmentId=${request.environmentId}`,
+      request,
+      headers
+    )
+  }
+
+  static async deleteSecret(
+    request: DeleteSecretRequest,
+    headers?: Record<string, string>
+  ): Promise<DeleteSecretResponse> {
+    return this.apiClient.delete(`/api/secret/${request.secretId}`, headers)
+  }
+
+  static async getAllSecretsOfProject(
+    request: getAllSecretsOfProjectRequest,
+    headers?: Record<string, string>
+  ): Promise<getAllSecretsOfProjectResponse> {
+    return this.apiClient.get(
+      `/api/secret/${request.projectId}?decryptValue=true`,
+      headers
+    )
+  }
+
+  static async getAllSecretsOfEnvironment(
+    request: getAllSecretsOfEnvironmentRequest,
+    headers?: Record<string, string>
+  ): Promise<getAllSecretsOfEnvironmentResponse> {
+    return this.apiClient.get(
+      `}/api/secret/${request.projectId}/${request.environmentId}`,
+      headers
+    )
+  }
 }
--- packages/api-client/src/types/secret.types.d.ts
+++ packages/api-client/src/types/secret.types.d.ts
@@ -1,12 +1,52 @@
-export interface CreateSecretRequest {}
-export interface CreateSecretResponse {}
-export interface UpdateSecretRequest {}
-export interface UpdateSecretResponse {}
-export interface DeleteSecretRequest {}
+export interface CreateSecretRequest {
+  projectId: string
+  name: string
+  entries: Entries[]
+}
+
+export interface Entries {
+  value: string
+  environmentId: string
+}
+export interface CreateSecretResponse {
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
+  rotateAt: string | null
+  note: string | null
+  lastUpdatedById: string
+  projectId: string
+  project: Project
+  versions: Entries[]
+}
+
+export interface Project {
+  workspaceId: string
+}
+export interface UpdateSecretRequest {
+  secretId: string
+}
+export interface UpdateSecretResponse {
+  secret: any
+  updatedVersions: any
+}
+export interface DeleteSecretRequest {
+  secretId: string
+}
 export interface DeleteSecretResponse {}
-export interface RollBackSecretRequest {}
+export interface RollBackSecretRequest {
+  environmentId: string
+  version: number
+  secretId: string
+}
 export interface RollBackSecretResponse {}
-export interface getAllSecretsOfProjectRequest {}
+export interface getAllSecretsOfProjectRequest {
+  projectId: string
+}
 export interface getAllSecretsOfProjectResponse {}
-export interface getAllSecretsOfEnvironmentRequest {}
+export interface getAllSecretsOfEnvironmentRequest {
+  projectId: string
+  environmentId: string
+}
 export interface getAllSecretsOfEnvironmentResponse {}
