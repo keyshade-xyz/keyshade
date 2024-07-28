a17e2c0c5064237b9ee1048ed4d315f1ac7007b5
vr-varad <varadgupta21#gmail.com>
2024-07-28 13:49:00 +0530

    Initialized Solution with structure

---
+++ packages/api-client/src/controllers/integration/integration.ts
@@ -0,0 +1,11 @@
+import client from '../../client'
+
+export default class IntegrationController {
+  private static apiClient = client
+
+  static async createIntegration() {}
+  static async updateIntegration() {}
+  static async getIntegration() {}
+  static async getAllIntegrations() {}
+  static async deleteIntegration() {}
+}
---
+++ packages/api-client/src/types/integrayion.types.d.ts
@@ -0,0 +1,19 @@
+export interface CreateIntegrationRequest {}
+
+export interface CreateIntegrationResponse {}
+
+export interface UpdateIntegrationRequest {}
+
+export interface UpdateIntegrationResponse {}
+
+export interface DeleteIntegrationResponse {}
+
+export interface DeleteIntegrationRequest {}
+
+export interface GetIntegrationRequest {}
+
+export interface GetIntegrationResponse {}
+
+export interface GetAllIntegrationRequest {}
+
+export interface GetAllIntegrationResponse {}
