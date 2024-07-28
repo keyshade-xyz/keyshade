1c4e67c2778e649c0bf3ab2903b4ea7bc37915aa
vr-varad <varadgupta21#gmail.com>
2024-07-28 09:11:45 +0530

    Defined Controllers and Types for req and res

--- packages/api-client/src/controllers/variable/variable.ts
+++ packages/api-client/src/controllers/variable/variable.ts
@@ -1,12 +1,71 @@
 import client from '@package/client'
+import {
+  CreateVariableRequest,
+  CreateVariableResponse,
+  DeleteVariableRequest,
+  DeleteVariableResponse,
+  getAllVariablesOfEnvironmentRequest,
+  getAllVariablesOfEnvironmentResponse,
+  getAllVariablesOfProjectRequest,
+  getAllVariablesOfProjectResponse,
+  RollBackVariableRequest,
+  RollBackVariableResponse,
+  UpdateVariableRequest,
+  UpdateVariableResponse
+} from '@package/types/variable.types'
 
 export default class VariableController {
   private static apiClient = client
 
-  static async createVariable() {}
-  static async updateVariable() {}
-  static async rollbackVariable() {}
-  static async deleteVariable() {}
-  static async getAllVariablesOfProject() {}
-  static async getAllVariablesOfEnvironment() {}
+  static async createVariable(
+    request: CreateVariableRequest,
+    headers: any
+  ): Promise<CreateVariableResponse> {
+    return this.apiClient.post(
+      `/api/variable/${request.projectId}`,
+      request,
+      headers
+    )
+  }
+  static async updateVariable(
+    request: UpdateVariableRequest,
+    headers: any
+  ): Promise<UpdateVariableResponse> {
+    return this.apiClient.put(
+      `/api/variable/${request.variableId}`,
+      request,
+      headers
+    )
+  }
+  static async rollbackVariable(
+    request: RollBackVariableRequest,
+    headers: any
+  ): Promise<RollBackVariableResponse> {
+    return this.apiClient.put(
+      `/api/variable/${request.variableId}/rollback/${request.version}?environmentId=${request.environmentId}`,
+      request,
+      headers
+    )
+  }
+  static async deleteVariable(
+    request: DeleteVariableRequest,
+    headers: any
+  ): Promise<DeleteVariableResponse> {
+    return this.apiClient.delete(`/api/variable/${request.variableId}`, headers)
+  }
+  static async getAllVariablesOfProject(
+    request: getAllVariablesOfProjectRequest,
+    headers: any
+  ): Promise<getAllVariablesOfProjectResponse> {
+    return this.apiClient.get(`}/api/variable/${request.projectId}`, headers)
+  }
+  static async getAllVariablesOfEnvironment(
+    request: getAllVariablesOfEnvironmentRequest,
+    headers: any
+  ): Promise<getAllVariablesOfEnvironmentResponse> {
+    return this.apiClient.get(
+      `/api/variable/${request.projectId}/${request.environmentId}`,
+      headers
+    )
+  }
 }
--- packages/api-client/src/types/variable.types.d.ts
+++ packages/api-client/src/types/variable.types.d.ts
@@ -1,12 +1,58 @@
-export interface CreateVariableRequest {}
-export interface CreateVariableResponse {}
-export interface UpdateVariableRequest {}
-export interface UpdateVariableResponse {}
-export interface RollBackVariableRequest {}
+export interface CreateVariableRequest {
+  projectId: string
+  name: string
+  entries: Entries[]
+}
+export interface Entries {
+  value: string
+  environmentId: string
+}
+export interface CreateVariableResponse {
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
+  note: string | null
+  lastUpdatedById: string
+  projectId: string
+  project: Project
+  versions: Entries[]
+}
+
+export interface Project {
+  workspaceId: string
+}
+export interface UpdateVariableRequest {
+  variableId: string
+  name?: string
+  entries?: Entries[]
+}
+export interface UpdateVariableResponse {
+  variables: Variable
+  updatedVersions: Entries[]
+}
+
+export interface Variable {
+  id: string
+  name: string
+  note: string
+}
+export interface RollBackVariableRequest {
+  variableId: string
+  version: string
+  environmentId: string
+}
 export interface RollBackVariableResponse {}
-export interface DeleteVariableRequest {}
+export interface DeleteVariableRequest {
+  variableId: string
+}
 export interface DeleteVariableResponse {}
-export interface getAllVariablesOfProjectRequest {}
+export interface getAllVariablesOfProjectRequest {
+  projectId: string
+}
 export interface getAllVariablesOfProjectResponse {}
-export interface getAllVariablesOfEnvironmentRequest {}
+export interface getAllVariablesOfEnvironmentRequest {
+  projectId: string
+  environmentId: string
+}
 export interface getAllVariablesOfEnvironmentResponse {}
