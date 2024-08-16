f8c74966169b24a56d10d6b045ba9732bb882e5d
Sambit Chakraborty <smbtchakraborty@gmail.com>
2024-08-16 22:28:00 +0530

    Add: Adafruit, adobe

---
+++ packages/secret-scan/src/rules/adafruit.ts
@@ -0,0 +1,23 @@
+// keyshade-ignore-all
+import type { TestCase } from '@/types'
+
+export default function adafruit(): RegExp[] {
+  return [/adafruit[a-zA-Z0-9]{32}/] // Adafruit secret keys are 32 characters long
+}
+
+const testcase: TestCase[] = [
+  {
+    input: 'adafruitGBWoV0UAltMF1nOG5T7sjFDZHrwoX7XN',
+    expected: true
+  },
+  {
+    input: 'adafruit1234567890123456789012345678',
+    expected: false
+  },
+  {
+    input: 'adafruit123',
+    expected: false
+  }
+]
+
+adafruit.testcases = testcase
