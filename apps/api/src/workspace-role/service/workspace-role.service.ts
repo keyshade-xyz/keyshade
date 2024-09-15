b563185661d214b7b6de4002c0a02f874a9d2e8e
NIL2000 <nilabhra09b.net@gmail.com>
2024-09-15 11:01:12 +0530

    fix(API): Replace projectId with name and slug in workspace-role response

--- apps/api/src/workspace-role/service/workspace-role.service.ts
+++ apps/api/src/workspace-role/service/workspace-role.service.ts
@@ -97,7 +97,13 @@ export class WorkspaceRoleService {
         include: {
           projects: {
             select: {
-              projectId: true
+              projectId: true,
+              project: {
+                select: {
+                  slug: true,
+                  name: true
+                }
+              }
             }
           }
         }
@@ -228,7 +234,13 @@ export class WorkspaceRoleService {
       include: {
         projects: {
           select: {
-            projectId: true
+            projectId: true,
+            project: {
+              select: {
+                slug: true,
+                name: true
+              }
+            }
           }
         }
       }
@@ -443,7 +455,17 @@ export class WorkspaceRoleService {
         slug: workspaceRoleSlug
       },
       include: {
-        projects: true
+        projects: {
+          select: {
+            projectId: true,
+            project: {
+              select: {
+                slug: true,
+                name: true
+              }
+            }
+          }
+        }
       }
     })) as WorkspaceRoleWithProjects
 
