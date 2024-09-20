84e2fcf0f0d856261463f215e6232ac93bbde80a
NIL2000 <nilabhra09b.net@gmail.com>
2024-09-20 13:17:05 +0530

    feat(cli): Implemented pagination support

--- apps/cli/src/commands/environment/list.environment.ts
+++ apps/cli/src/commands/environment/list.environment.ts
@@ -1,6 +1,7 @@
 import BaseCommand from '../base.command'
-import { EnvironmentController } from '@keyshade/api-client'
+import { EnvironmentController } from '@keyshade/api-client/src'
 import {
+  CommandOption,
   type CommandActionData,
   type CommandArgument
 } from 'src/types/command/command.types'
@@ -24,9 +25,39 @@ export class ListEnvironment extends BaseCommand {
     ]
   }
 
-  async action({ args }: CommandActionData): Promise<void> {
+  getOptions(): CommandOption[] {
+    return [
+      {
+        short: '-p',
+        long: '--page <page>',
+        description: 'Index of the page.'
+      },
+      {
+        short: '-l',
+        long: '--limit <items>',
+        description: 'Total number of items per page.'
+      },
+      {
+        short: '-o',
+        long: '--order <sort order>',
+        description:
+          'Order to sort by - either ascending (ASC) or descending (DESC).'
+      },
+      {
+        short: '--sort',
+        long: '--sort <field name>',
+        description: 'Field to sort by.'
+      },
+      {
+        short: '-s',
+        long: '--search <search>',
+        description: 'Search term.'
+      }
+    ]
+  }
+  async action({ args, options }: CommandActionData): Promise<void> {
     const [projectSlug] = args
-
+    const { page, limit, order, sort, search } = options
     if (!projectSlug) {
       Logger.error('Project slug is required')
       return
@@ -44,7 +75,7 @@ export class ListEnvironment extends BaseCommand {
       data: environments,
       error
     } = await environmentController.getAllEnvironmentsOfProject(
-      { projectSlug },
+      { projectSlug, page, limit, order, sort, search },
       headers
     )
 
--- apps/cli/src/commands/workspace/list.workspace.ts
+++ apps/cli/src/commands/workspace/list.workspace.ts
@@ -1,6 +1,7 @@
 import BaseCommand from '@/commands/base.command'
 import { Logger } from '@/util/logger'
 import ControllerInstance from '@/util/controller-instance'
+import { CommandActionData, CommandOption } from '@/types/command/command.types'
 
 export default class ListWorkspace extends BaseCommand {
   getName(): string {
@@ -11,12 +12,51 @@ export default class ListWorkspace extends BaseCommand {
     return 'Fetches all the workspace you have access to'
   }
 
-  async action(): Promise<void> {
+  getOptions(): CommandOption[] {
+    return [
+      {
+        short: '-p',
+        long: '--page <page>',
+        description: 'Index of the page.'
+      },
+      {
+        short: '-l',
+        long: '--limit <items>',
+        description: 'Total number of items per page.'
+      },
+      {
+        short: '-o',
+        long: '--order <sort order>',
+        description:
+          'Order to sort by - either ascending (ASC) or descending (DESC).'
+      },
+      {
+        short: '--sort',
+        long: '--sort <field name>',
+        description: 'Field to sort by.'
+      },
+      {
+        short: '-s',
+        long: '--search <search>',
+        description: 'Search term.'
+      }
+    ]
+  }
+
+  async action({ options }: CommandActionData): Promise<void> {
     Logger.info('Fetching all workspaces...')
+
+    const { page, limit, order, sort, search } = options
 
     const { success, data, error } =
       await ControllerInstance.getInstance().workspaceController.getWorkspacesOfUser(
-        {},
+        {
+          page,
+          limit,
+          order,
+          sort,
+          search
+        },
         this.headers
       )
 
