feb1806e30ee609e06bb0254040414b775f55606
Anonymus2000 <54113952+Nil2000@users.noreply.github.com>
2024-09-21 19:15:45 +0530

    feat(cli): Implemented pagination support (#453)

---
+++ apps/cli/src/util/pagination-options.ts
@@ -0,0 +1,31 @@
+import { CommandOption } from '@/types/command/command.types'
+
+export const PAGINATION_OPTION: CommandOption[] = [
+  {
+    short: '-p',
+    long: '--page <int>',
+    description: 'Index of the page.'
+  },
+  {
+    short: '-l',
+    long: '--limit <int>',
+    description: 'Total number of items per page.'
+  },
+  {
+    short: '-o',
+    long: '--order <string>',
+    description:
+      'Order to sort by - either ascending (ASC) or descending (DESC).',
+    choices: ['ASC', 'DESC']
+  },
+  {
+    short: '--sort',
+    long: '--sort <string>',
+    description: 'Field to sort by.'
+  },
+  {
+    short: '-s',
+    long: '--search <string>',
+    description: 'Search term.'
+  }
+]
