ea69bc22fc7299be1dbfd6818ced075a0b245a43
NIL2000 <nilabhra09b.net@gmail.com>
2024-09-29 23:20:33 +0530

    Testing typo fix

--- apps/api/src/variable/variable.e2e.spec.ts
+++ apps/api/src/variable/variable.e2e.spec.ts
@@ -53,6 +53,7 @@ describe('Variable Controller Tests', () => {
   let project1: Project
   let environment1: Environment
   let environment2: Environment
+  let environment3: Environment
   let variable1: Variable
 
   beforeAll(async () => {
@@ -124,6 +125,10 @@ describe('Variable Controller Tests', () => {
         {
           name: 'Environment 2',
           description: 'Environment 2 description'
+        },
+        {
+          name: 'Test Environment',
+          description: 'Test environment description'
         }
       ]
     })) as Project
@@ -139,6 +144,13 @@ describe('Variable Controller Tests', () => {
       where: {
         projectId: project1.id,
         name: 'Environment 2'
+      }
+    })
+
+    environment3 = await prisma.environment.findFirst({
+      where: {
+        projectId: project1.id,
+        name: 'Test Environment'
       }
     })
 
@@ -184,7 +196,7 @@ describe('Variable Controller Tests', () => {
           entries: [
             {
               value: 'Variable 3 value',
-              environmentId: environment2.id
+              environmentId: environment3.id
             }
           ]
         },
@@ -205,7 +217,7 @@ describe('Variable Controller Tests', () => {
       expect(body.versions.length).toBe(1)
       expect(body.versions[0].value).toBe('Variable 3 value')
       // expect(body.versions[0].environment.id).toBe(environment2.id)
-      expect(body.versions[0].environment.slug).toBe(environment2.slug)
+      expect(body.versions[0].environment.slug).toBe(environment3.slug)
 
       const variable = await prisma.variable.findUnique({
         where: {
