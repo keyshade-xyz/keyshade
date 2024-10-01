6358ab16907663efe808b5631681eba4d277bc8f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-10-01 19:35:10 +0300

    feat(cli): implement commands to get, list, update, and delete, workspace roles (#469)
    Signed-off-by: Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>

---
+++ apps/cli/src/commands/workspace/role/delete.role.ts
@@ -0,0 +1,44 @@
+import BaseCommand from '@/commands/base.command'
+import {
+  type CommandActionData,
+  type CommandArgument
+} from '@/types/command/command.types'
+import { Logger } from '@/util/logger'
+import ControllerInstance from '@/util/controller-instance'
+
+export default class DeleteRoleCommand extends BaseCommand {
+  getName(): string {
+    return 'delete'
+  }
+
+  getDescription(): string {
+    return 'Delete workspace role'
+  }
+
+  getArguments(): CommandArgument[] {
+    return [
+      {
+        name: '<Workspace Role Slug>',
+        description: 'Slug of the workspace role you want to delete.'
+      }
+    ]
+  }
+
+  async action({ args }: CommandActionData): Promise<void> {
+    const [workspaceRoleSlug] = args
+
+    const { error, success } =
+      await ControllerInstance.getInstance().workspaceRoleController.deleteWorkspaceRole(
+        {
+          workspaceRoleSlug
+        },
+        this.headers
+      )
+
+    if (success) {
+      Logger.info(`Workspace role ${workspaceRoleSlug} deleted successfully!`)
+    } else {
+      Logger.error(`Failed deleting workspace role: ${error.message}`)
+    }
+  }
+}
