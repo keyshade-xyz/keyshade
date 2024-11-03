46eb5c5aeee76f60015e65174e9cb9ea97f5e771
Prakhar Garg <23prakhargarg2002@gmail.com>
2024-11-03 09:50:08 +0530

    feat(platform): Create ui link for resend otp (#489)
    Co-authored-by: rajdip-b <agentR47@gmail.com>

---
+++ apps/platform/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['custom/next'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off'
+  }
+}
---
+++ packages/api-client/src/controllers/auth.ts
@@ -0,0 +1,27 @@
+import {
+  ResendOTPResponse,
+  ResendOTPRequest
+} from '@api-client/types/auth.types'
+import { APIClient } from '@api-client/core/client'
+import { parseResponse } from '@api-client/core/response-parser'
+import { ClientResponse } from '@api-client/types/index.types'
+
+export default class AuthController {
+  private apiClient: APIClient
+
+  constructor(private readonly backendURL: string) {
+    this.apiClient = new APIClient(this.backendURL)
+  }
+
+  async resendOTP(
+    request: ResendOTPRequest,
+    headers?: Record<string, string>
+  ): Promise<ClientResponse<ResendOTPResponse>> {
+    const response = await this.apiClient.post(
+      `/api/auth/resend-otp/${request.userEmail}`,
+      request,
+      headers
+    )
+    return await parseResponse<ResendOTPResponse>(response)
+  }
+}
