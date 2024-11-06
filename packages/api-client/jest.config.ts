5b27e32cc76b24a3f7f4f99ee0917456ecba05ad
rajdip-b <agentR47@gmail.com>
2024-11-06 22:39:18 +0530

    chore: Update test coverage settings

--- packages/api-client/jest.config.ts
+++ packages/api-client/jest.config.ts
@@ -7,14 +7,16 @@ const config: Config = {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/api-client',
   globalSetup: './tests/config/setup.ts',
   globalTeardown: './tests/config/teardown.ts',
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@api-client/(.*)$': '<rootDir>/src/$1'
-  }
+  },
+  coverageDirectory: '../../coverage/packages/api-client',
+  coverageReporters: ['json'],
+  collectCoverage: true
 }
 
 export default config
