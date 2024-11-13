ca8bf761ddd7f5d438c9ae50d551d625fa82c460
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-11-13 17:55:25 +0530

    test(secret-scan): Add test coverage

--- packages/secret-scan/src/test/secret.test.ts
+++ packages/secret-scan/src/test/secret.test.ts
@@ -1,5 +1,3 @@
-import { describe, it } from 'mocha'
-import assert = require('assert')
 import {
   algolia,
   alibaba,
@@ -71,16 +69,13 @@ import {
 import type { TestCase } from '@/types'
 import secretDetector from '@/index'
 
-const testcaseTitleTemplate = (title: string) => `should detect ${title}`
+const testcaseTitleTemplate = (title: string): string =>
+  `should detect ${title}`
 
-function testSecret(testcase: TestCase[]) {
-  testcase.forEach(({ input, expected }, index) => {
+function testSecret(testcases: TestCase[]): void {
+  testcases.forEach(({ input, expected }, index) => {
     const result = secretDetector.detect(input)
-    assert.equal(
-      result.found,
-      expected,
-      `(i=${index}) For Input: ${input}, expected ${expected} but got ${result.found}`
-    )
+    expect(result.found).toBe(expected)
   })
 }
 
@@ -125,7 +120,6 @@ describe('Detect Secrets from string', () => {
     testSecret(mailchimp.testcases)
   })
 
-  // ! not working for seperated by dots, saying true to all
   // it(testcaseTitleTemplate("JWT Key"), () => {
   //     testSecret(jwt.testcases);
   // });
@@ -141,48 +135,63 @@ describe('Detect Secrets from string', () => {
   it(testcaseTitleTemplate('Discord Token'), () => {
     testSecret(discord.testcases)
   })
+
   it(testcaseTitleTemplate('Artifactory Token'), () => {
     testSecret(artifactory.testcases)
   })
+
   it(testcaseTitleTemplate('AWS Token'), () => {
     testSecret(aws.testcases)
   })
+
   it(testcaseTitleTemplate('Algolia Token'), () => {
     testSecret(algolia.testcases)
   })
+
   it(testcaseTitleTemplate('Alibaba Key'), () => {
     testSecret(alibaba.testcases)
   })
+
   it(testcaseTitleTemplate('Adafruit Key'), () => {
     testSecret(adafruit.testcases)
   })
+
   it(testcaseTitleTemplate('Adobe Key'), () => {
     testSecret(adobe.testcases)
   })
+
   it(testcaseTitleTemplate('Age Key'), () => {
     testSecret(age.testcases)
   })
+
   it(testcaseTitleTemplate('Airtable Key'), () => {
     testSecret(airtable.testcases)
   })
+
   it(testcaseTitleTemplate('Asana Key'), () => {
     testSecret(asana.testcases)
   })
+
   it(testcaseTitleTemplate('Atlassian Key'), () => {
     testSecret(atlassian.testcases)
   })
+
   it(testcaseTitleTemplate('Authress Key'), () => {
     testSecret(authress.testcases)
   })
+
   it(testcaseTitleTemplate('Beamer Key'), () => {
     testSecret(beamer.testcases)
   })
+
   it(testcaseTitleTemplate('Bitbucket Key'), () => {
     testSecret(bitbucket.testcases)
   })
+
   it(testcaseTitleTemplate('Bittrex Key'), () => {
     testSecret(bittrex.testcases)
   })
+
   it(testcaseTitleTemplate('Clojars Key'), () => {
     testSecret(clojars.testcases)
   })
@@ -190,117 +199,155 @@ describe('Detect Secrets from string', () => {
   it(testcaseTitleTemplate('Codecov Key'), () => {
     testSecret(codecov.testcases)
   })
+
   it(testcaseTitleTemplate('Coinbase Key'), () => {
     testSecret(coinbase.testcases)
   })
+
   it(testcaseTitleTemplate('Confluent Key'), () => {
     testSecret(confluent.testcases)
   })
+
   it(testcaseTitleTemplate('Contentful Key'), () => {
     testSecret(contentful.testcases)
   })
+
   it(testcaseTitleTemplate('Databricks Key'), () => {
     testSecret(databricks.testcases)
   })
+
   it(testcaseTitleTemplate('Datadog Key'), () => {
     testSecret(datadog.testcases)
   })
+
   it(testcaseTitleTemplate('Defined Networking Key'), () => {
     testSecret(definednetworking.testcases)
   })
+
   it(testcaseTitleTemplate('Digital Ocean Key'), () => {
     testSecret(digitalocean.testcases)
   })
+
   it(testcaseTitleTemplate('Doppler Key'), () => {
     testSecret(doppler.testcases)
   })
+
   it(testcaseTitleTemplate('Dropbox Key'), () => {
     testSecret(dropbox.testcases)
   })
+
   it(testcaseTitleTemplate('Duffel Key'), () => {
     testSecret(duffel.testcases)
   })
+
   it(testcaseTitleTemplate('Dynatrace Key'), () => {
     testSecret(dynatrace.testcases)
   })
+
   it(testcaseTitleTemplate('Easypost Key'), () => {
     testSecret(easypost.testcases)
   })
+
   it(testcaseTitleTemplate('Facebook Key'), () => {
     testSecret(facebook.testcases)
   })
+
   it(testcaseTitleTemplate('Flutterwave Key'), () => {
     testSecret(flutterwave.testcases)
   })
+
   it(testcaseTitleTemplate('Frameio Key'), () => {
     testSecret(frameio.testcases)
   })
+
   it(testcaseTitleTemplate('Gitlab Key'), () => {
     testSecret(gitlab.testcases)
   })
+
   it(testcaseTitleTemplate('Grafana Key'), () => {
     testSecret(grafana.testcases)
   })
+
   it(testcaseTitleTemplate('Harness Key'), () => {
     testSecret(harness.testcases)
   })
+
   it(testcaseTitleTemplate('Hashicorp Key'), () => {
     testSecret(hashicorp.testcases)
   })
+
   it(testcaseTitleTemplate('Heroku Key'), () => {
     testSecret(heroku.testcases)
   })
+
   it(testcaseTitleTemplate('Hubspot Key'), () => {
     testSecret(hubspot.testcases)
   })
+
   it(testcaseTitleTemplate('Huggingface Key'), () => {
     testSecret(huggingface.testcases)
   })
+
   it(testcaseTitleTemplate('Infracost Key'), () => {
     testSecret(infracost.testcases)
   })
+
   it(testcaseTitleTemplate('Intra42 Key'), () => {
     testSecret(intra42.testcases)
   })
+
   // it(testcaseTitleTemplate('Kubernetes Key'), () => {
-  //   testSecret(kubernetes.testcases)
+  //   testSecret(kubernetes.testcases);
   // });
+
   it(testcaseTitleTemplate('Linear Key'), () => {
     testSecret(linear.testcases)
   })
+
   it(testcaseTitleTemplate('Lob Key'), () => {
     testSecret(lob.testcases)
   })
+
   it(testcaseTitleTemplate('Planetscale Key'), () => {
     testSecret(planetscale.testcases)
   })
+
   it(testcaseTitleTemplate('Postman Key'), () => {
     testSecret(postman.testcases)
   })
+
   it(testcaseTitleTemplate('Prefect Key'), () => {
     testSecret(prefect.testcases)
   })
+
   it(testcaseTitleTemplate('Pulumi Key'), () => {
     testSecret(pulumi.testcases)
   })
+
   it(testcaseTitleTemplate('Readme Key'), () => {
     testSecret(readme.testcases)
   })
+
   it(testcaseTitleTemplate('Rubygems Key'), () => {
     testSecret(rubygems.testcases)
   })
+
   it(testcaseTitleTemplate('Scalingo Key'), () => {
     testSecret(scalingo.testcases)
   })
+
   it(testcaseTitleTemplate('Sendinblue Key'), () => {
     testSecret(sendinblue.testcases)
   })
+
   it(testcaseTitleTemplate('Shippo Key'), () => {
     testSecret(shippo.testcases)
   })
+
   it(testcaseTitleTemplate('Shopify Key'), () => {
     testSecret(shopify.testcases)
   })
+
   it(testcaseTitleTemplate('Sidekiq Key'), () => {
     testSecret(sidekiq.testcases)
   })
