5c6a621446bfe0f135f419802aaa9f9ae41baae5
muntaxir4 <mjmullick0608@gmail.com>
2024-12-02 05:03:15 +0530

    feat: add GitHubScanner class

--- apps/github-scan/src/app/api/scan/route.ts
+++ apps/github-scan/src/app/api/scan/route.ts
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { secret_scan } from '@/util/scan_secrets'
+import gitHubScanner from '@/util/github-scanner'
 
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
-    const github_url = searchParams.get('github_url')
+    const githubUrl = searchParams.get('github_url')
 
-    if (!github_url) {
+    if (!githubUrl) {
       return NextResponse.json(
         { error: 'Invalid or missing github_url parameter' },
         { status: 400 }
       )
     }
-
-    const result = await secret_scan(github_url)
-    if (result instanceof Array) {
+    try {
+      const result = await gitHubScanner.scanRepo(githubUrl)
       if (result.length > 0)
         return NextResponse.json(
           { isVulnerable: true, files: result },
@@ -27,7 +26,23 @@ export async function GET(req: NextRequest) {
           },
           { status: 200 }
         )
-    } else return result
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message === 'Rate limit hit') {
+          return NextResponse.json({ error: 'Rate limit hit' }, { status: 429 })
+        } else if (error.message === 'Invalid GitHub URL') {
+          return NextResponse.json(
+            { error: 'Invalid GitHub URL' },
+            { status: 400 }
+          )
+        } else {
+          return NextResponse.json(
+            { error: 'Internal Server Error' },
+            { status: 500 }
+          )
+        }
+      }
+    }
   } catch (error) {
     return NextResponse.json(
       { error: 'Internal Server Error' },
