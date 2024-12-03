2205cb4018067115a15fd32e466a3fc602a84227
rajdip-b <agentR47@gmail.com>
2024-12-04 00:50:53 +0530

    incorporated user from schema package

--- apps/platform/.eslintrc.cjs
+++ apps/platform/.eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off'
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unnecessary-condition': 'off'
   }
 }
