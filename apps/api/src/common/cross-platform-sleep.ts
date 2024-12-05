3ba51a24590c418fe5b8daa6156f3b4f83ceff9a
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-06 00:16:14 +0530

    fix: used nestjs logger & used ts-node to transpile & run ts file

--- apps/api/src/common/cross-platform-sleep.ts
+++ apps/api/src/common/cross-platform-sleep.ts
@@ -1,22 +1,26 @@
-import { execSync } from 'child_process';
-import { platform } from 'os';
+import { execSync } from 'child_process'
+import { platform } from 'os'
+import { Logger } from '@nestjs/common'
 
-const seconds = 3;
-const os = platform();
+const logger = new Logger('SleepCommand')
+const seconds = 3
+const os = platform()
 
-let command: string;
+let command: string
 if (os === 'win32') {
-  command = `powershell -command "Start-Sleep -s ${seconds}"`;
+  command = `powershell -command "Start-Sleep -s ${seconds}"`
 } else if (os === 'darwin' || os === 'linux') {
-  command = `sleep ${seconds}`;
+  command = `sleep ${seconds}`
 } else {
-  console.error('Unsupported operating system');
-  process.exit(1);
+  logger.error('Unsupported operating system')
+  process.exit(1)
 }
 
 try {
-  execSync(command);
+  logger.log(`Executing sleep command for ${seconds} seconds on ${os}`)
+  execSync(command)
+  logger.log('Sleep command executed successfully')
 } catch (error) {
-  console.error(`Error executing sleep command: ${error}`);
-  process.exit(1);
+  logger.error(`Error executing sleep command: ${error}`)
+  process.exit(1)
 }
