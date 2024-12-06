f609d29d96cad3b3a4ee3a8aa4cf4f2d34a0c46c
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-06 21:29:15 +0530

    feat: created a reusable base email template component

--- apps/api/src/mail/emails/workspace-invitation.tsx
+++ apps/api/src/mail/emails/workspace-invitation.tsx
@@ -1,30 +1,13 @@
 import * as React from 'react'
-import {
-  Body,
-  Button,
-  Container,
-  Head,
-  Heading,
-  Html,
-  Link,
-  Preview,
-  Section,
-  Text
-} from '@react-email/components'
+import { Button, Section, Text } from '@react-email/components'
 import dayjs from 'dayjs'
 import {
-  container,
-  content,
   ctaButton,
-  footer,
-  footerText,
-  h1,
-  link,
-  main,
   text,
   workspaceDetails,
   workspaceInfo
-} from '../styles/common-styles'
+} from './styles/common-styles'
+import BaseEmailTemplate from './components/base-email-template'
 
 interface WorkspaceInvitationEmailProps {
   workspaceName: string
@@ -45,85 +28,39 @@ export const WorkspaceInvitationEmail = ({
     'ddd, MMM D, YYYY h:mm A'
   )
 
+  const previewText = forRegisteredUser
+    ? 'Welcome Back! Join Your Workspace'
+    : 'You are Invited to Join the Workspace'
+
   return (
-    <Html>
-      <Head />
-      <Preview>
-        {forRegisteredUser
-          ? 'Welcome Back! Join Your Workspace'
-          : 'You are Invited to Join the Workspace'}
-      </Preview>
-      <Body style={main}>
-        <Container style={container}>
-          <Section style={content}>
-            <Heading style={h1}>
-              {forRegisteredUser
-                ? 'Welcome Back! Join Your Workspace'
-                : 'You are Invited to Join the Workspace'}
-            </Heading>
-            <Text style={text}>Dear User,</Text>
-            <Text style={text}>
-              We're excited to inform you that you've been invited to join a
-              project on Keyshade. Here are the details of your invitation:
-            </Text>
-            <Section style={workspaceDetails}>
-              <Text style={workspaceInfo}>
-                <strong>Workspace Name:</strong> {workspaceName}
-              </Text>
-              <Text style={workspaceInfo}>
-                <strong>Invited By:</strong> {invitedBy}
-              </Text>
-              <Text style={workspaceInfo}>
-                <strong>Invited On:</strong> {invitedOn}
-              </Text>
-              <Text style={workspaceInfo}>
-                <strong>Invited On:</strong> {formattedInvitedOnDate}
-              </Text>
-            </Section>
-            <Text style={text}>
-              Join the project by clicking the button below - we're excited to
-              have you!
-            </Text>
-            <Button href={actionUrl} style={ctaButton}>
-              Get started
-            </Button>
-            <Text style={text}>
-              If you believe this action was taken in error or have any
-              questions regarding this change, please contact your project
-              administrator or our support team.
-            </Text>
-            <Text style={text}>
-              We appreciate your understanding and thank you for your
-              contributions to the project.
-            </Text>
-            <Text style={text}>
-              Cheers,
-              <br />
-              Team Keyshade
-            </Text>
-          </Section>
-          <Section style={footer}>
-            <Text style={footerText}>
-              This is an automated message. Please do not reply to this email.
-            </Text>
-            <Text style={footerText}>
-              Read our{' '}
-              <Link href="https://www.keyshade.xyz/privacy" style={link}>
-                Privacy Policy
-              </Link>{' '}
-              and{' '}
-              <Link
-                href="https://www.keyshade.xyz/terms_and_condition"
-                style={link}
-              >
-                Terms and Conditions
-              </Link>{' '}
-              for more information on how we manage your data and services.
-            </Text>
-          </Section>
-        </Container>
-      </Body>
-    </Html>
+    <BaseEmailTemplate
+      previewText={previewText}
+      heading={previewText}
+    >
+      <Text style={text}>Dear User,</Text>
+      <Text style={text}>
+        We're excited to inform you that you've been invited to join a
+        project on Keyshade. Here are the details of your invitation:
+      </Text>
+      <Section style={workspaceDetails}>
+        <Text style={workspaceInfo}>
+          <strong>Workspace Name:</strong> {workspaceName}
+        </Text>
+        <Text style={workspaceInfo}>
+          <strong>Invited By:</strong> {invitedBy}
+        </Text>
+        <Text style={workspaceInfo}>
+          <strong>Invited On:</strong> {formattedInvitedOnDate}
+        </Text>
+      </Section>
+      <Text style={text}>
+        Join the project by clicking the button below - we're excited to
+        have you!
+      </Text>
+      <Button href={actionUrl} style={ctaButton}>
+        Get started
+      </Button>
+    </BaseEmailTemplate>
   )
 }
 
--- apps/api/src/mail/emails/workspace-removal.tsx
+++ apps/api/src/mail/emails/workspace-removal.tsx
@@ -1,28 +1,8 @@
 import * as React from 'react'
-import {
-  Body,
-  Container,
-  Head,
-  Heading,
-  Html,
-  Link,
-  Preview,
-  Section,
-  Text
-} from '@react-email/components'
+import { Text, Section } from '@react-email/components'
 import dayjs from 'dayjs'
-import {
-  container,
-  content,
-  footer,
-  footerText,
-  h1,
-  link,
-  main,
-  text,
-  workspaceDetails,
-  workspaceInfo
-} from '../styles/common-styles'
+import { text, workspaceDetails, workspaceInfo } from './styles/common-styles'
+import BaseEmailTemplate from './components/base-email-template'
 
 interface WorkspaceRemovalEmailProps {
   workspaceName: string
@@ -38,63 +18,24 @@ export const RemovedFromWorkspaceEmail = ({
   )
 
   return (
-    <Html>
-      <Head />
-      <Preview>Removal from Workspace</Preview>
-      <Body style={main}>
-        <Container style={container}>
-          <Section style={content}>
-            <Heading style={h1}>Removal from Workspace</Heading>
-            <Text style={text}>Dear User,</Text>
-            <Text style={text}>
-              We hope this email finds you well. We are writing to inform you
-              that your access to the following workspace has been removed:
-            </Text>
-            <Section style={workspaceDetails}>
-              <Text style={workspaceInfo}>
-                <strong>Workspace Name:</strong> {workspaceName}
-              </Text>
-              <Text style={workspaceInfo}>
-                <strong>Removed On:</strong> {formattedRemovedOnDate}
-              </Text>
-            </Section>
-            <Text style={text}>
-              If you believe this action was taken in error or have any
-              questions regarding this change, please contact your project
-              administrator or our support team.
-            </Text>
-            <Text style={text}>
-              We appreciate your understanding and thank you for your
-              contributions to the project.
-            </Text>
-            <Text style={text}>
-              Cheers,
-              <br />
-              Team Keyshade
-            </Text>
-          </Section>
-          <Section style={footer}>
-            <Text style={footerText}>
-              This is an automated message. Please do not reply to this email.
-            </Text>
-            <Text style={footerText}>
-              Read our{' '}
-              <Link href="https://www.keyshade.xyz/privacy" style={link}>
-                Privacy Policy
-              </Link>{' '}
-              and{' '}
-              <Link
-                href="https://www.keyshade.xyz/terms_and_condition"
-                style={link}
-              >
-                Terms and Conditions
-              </Link>{' '}
-              for more information on how we manage your data and services.
-            </Text>
-          </Section>
-        </Container>
-      </Body>
-    </Html>
+    <BaseEmailTemplate
+      previewText="Removal from Workspace"
+      heading="Removal from Workspace"
+    >
+      <Text style={text}>Dear User,</Text>
+      <Text style={text}>
+        We hope this email finds you well. We are writing to inform you
+        that your access to the following workspace has been removed:
+      </Text>
+      <Section style={workspaceDetails}>
+        <Text style={workspaceInfo}>
+          <strong>Workspace Name:</strong> {workspaceName}
+        </Text>
+        <Text style={workspaceInfo}>
+          <strong>Removed On:</strong> {formattedRemovedOnDate}
+        </Text>
+      </Section>
+    </BaseEmailTemplate>
   )
 }
 
