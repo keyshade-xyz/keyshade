7fd533fdd1538f812e5027501153fb1be3e8d7a9
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-12-09 18:47:46 +0530

    fix: Empty name () accepted as a valid name while creating environments

--- apps/api/src/environment/dto/create.environment/create.environment.ts
+++ apps/api/src/environment/dto/create.environment/create.environment.ts
@@ -1,8 +1,9 @@
-import {IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator'
 
 export class CreateEnvironment {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9-_]{1,64}$/)
   name: string
 
   @IsString()
