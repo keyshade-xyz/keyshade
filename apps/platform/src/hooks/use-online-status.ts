865bb2135706d64ea88707c97d378d4ff5a4c3d9
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-10 22:47:40 +0530

    fix: use timer ref to hold setTimeout id

--- apps/platform/src/hooks/use-online-status.ts
+++ apps/platform/src/hooks/use-online-status.ts
@@ -1,18 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { toast } from "sonner";
 
-const statusHandler = () => {
-    if (navigator.onLine) {
-        toast.success("You are back online! Refreshing...");
-        setTimeout(() => {
-            window.location.reload();
-        }, 1000);
-    } else {
-        toast.error("You are offline");
-    }
-};
-
 export const useOnlineStatus = () => {
+    const statusTimeout = useRef<NodeJS.Timeout | null>(null);
+
+    const statusHandler = () => {
+        if (statusTimeout.current) {
+            clearTimeout(statusTimeout.current);
+        }
+
+        statusTimeout.current = setTimeout(() => {
+            if (navigator.onLine) {
+                toast.success("You are back online! Refreshing...");
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
+            } else {
+                toast.error("You are offline");
+            }
+        }, 1000);
+    };
+
     useEffect(() => {
         window.addEventListener("online", statusHandler);
         window.addEventListener("offline", statusHandler);
