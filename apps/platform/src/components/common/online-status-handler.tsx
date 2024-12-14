0edd8cf015fea5dcf4642f6b1dc2bb93f30ce144
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2024-12-14 13:02:01 +0530

    feat(platofrm): Added online/offline status detection in the platform (#585)

---
+++ apps/platform/src/components/common/online-status-handler.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useOnlineStatus } from "@/hooks/use-online-status";
+
+function OnlineStatusHandler() {
+    useOnlineStatus();
+    return null;
+}
+
+export default OnlineStatusHandler;
