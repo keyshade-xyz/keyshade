ae314fafdac7a6b3afc7499bae889c8df0a4f54c
rajdip-b <agentR47@gmail.com>
2024-12-17 21:59:04 +0530

    finishing touches

--- apps/platform/.eslintrc.cjs
+++ apps/platform/.eslintrc.cjs
@@ -14,6 +14,10 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-misused-promises': 'off'
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off'
   }
 }
