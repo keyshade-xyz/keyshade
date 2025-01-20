e6e09b751fb02a6f0f527e246240adafbdd7117f
csehatt741 <att741@gmail.com>
2025-01-20 06:09:47 +0100

    fix(cli): API health probe added

---
+++ packages/api-client/src/controllers/app.ts
@@ -0,0 +1,16 @@
+import { APIClient } from '@api-client/core/client'
+
+export default class AppController {
+  private apiClient: APIClient
+
+  constructor(private readonly backendURL: string) {
+    this.apiClient = new APIClient(this.backendURL)
+  }
+
+  async health(
+    headers?: Record<string, string>
+  ): Promise<string> {
+    const response = await this.apiClient.get(`/api/health`, headers)
+    return response.text()
+  }
+}
