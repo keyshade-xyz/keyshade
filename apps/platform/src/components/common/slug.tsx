6bf6664f49a4ead5001ee0742bb09ca89d823a2b
rajdip-b <agentR47@gmail.com>
2025-01-29 23:58:29 +0530

    add mono font support

--- apps/platform/src/components/common/slug.tsx
+++ apps/platform/src/components/common/slug.tsx
@@ -1,7 +1,8 @@
-import { Roboto } from 'next/font/google'
+// eslint-disable-next-line camelcase -- this is a font import
+import { Roboto_Mono } from 'next/font/google'
 import { toast } from 'sonner'
 
-const roboto = Roboto({ weight: ['400'], subsets: ['latin'] })
+const roboto = Roboto_Mono({ weight: ['400'], subsets: ['latin'] })
 
 interface SlugProps {
   text: string
@@ -21,7 +22,7 @@ export default function Slug({ text }: SlugProps): React.JSX.Element {
 
   return (
     <button
-      className={`${roboto.className} rounded-lg bg-white/[10%] px-3 py-2 text-sm text-white/50`}
+      className={`${roboto.className} rounded-lg bg-white/[10%] px-3 py-2 font-mono text-sm text-white/50`}
       onClick={copyToClipboard}
       type="button"
     >
