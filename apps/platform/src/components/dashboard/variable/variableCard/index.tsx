378bea19bc18fc6323d8091682ca2c3eebf7b2d2
kriptonian1 <shymal.sawan@gmail.com>
2025-01-30 15:00:14 +0530

    refactor(variableCard): simplify component by removing unused imports and collapsing logic

--- apps/platform/src/components/dashboard/variable/variableCard/index.tsx
+++ apps/platform/src/components/dashboard/variable/variableCard/index.tsx
@@ -1,14 +1,7 @@
 import type { GetAllVariablesOfProjectResponse } from '@keyshade/schema'
-import { MessageSVG } from '@public/svg/shared'
-import { ChevronDown } from 'lucide-react'
-import { useState } from 'react'
 import { useSetAtom } from 'jotai'
 import dayjs from 'dayjs'
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger
-} from '@/components/ui/collapsible'
+import { NoteIconSVG } from '@public/svg/secret'
 import {
   Table,
   TableBody,
@@ -20,16 +13,13 @@ import {
 import {
   ContextMenu,
   ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger
-} from '@/components/ui/context-menu'
+  ContextMenuItem} from '@/components/ui/context-menu'
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger
 } from '@/components/ui/tooltip'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   deleteVariableOpenAtom,
   editVariableOpenAtom,
@@ -40,7 +30,6 @@ import {
   AccordionItem,
   AccordionTrigger
 } from '@/components/ui/accordion'
-import { NoteIconSVG } from '@public/svg/secret'
 
 export default function VariableCard(
   variableData: GetAllVariablesOfProjectResponse['items'][number]
@@ -61,105 +50,8 @@ export default function VariableCard(
     setIsDeleteVariableOpen(true)
   }
 
-  // Holds the currently open section ID
-  const [openSections, setOpenSections] = useState<Set<string>>(new Set())
-
-  //Environments table toggle logic
-  const toggleSection = (id: string) => {
-    setOpenSections((prev) => {
-      const newSet = new Set(prev)
-      if (newSet.has(id)) {
-        newSet.delete(id)
-      } else {
-        newSet.add(id)
-      }
-      return newSet
-    })
-  }
-
   return (
     <ContextMenu key={variable.id}>
-      {/* <ContextMenuTrigger className="w-full">
-        <Collapsible
-          className="w-full"
-          key={variable.id}
-          onOpenChange={() => toggleSection(variable.id)}
-          open={openSections.has(variable.id)}
-        >
-          <CollapsibleTrigger
-            className={`flex h-[6.75rem] w-full items-center justify-between gap-24 ${openSections.has(variable.id) ? 'rounded-t-xl' : 'rounded-xl'} bg-[#232424] px-4 py-2 text-left`}
-          >
-            <div className="flex h-[2.375rem] items-center justify-center gap-4">
-              <span className="h-[2.375rem] text-2xl font-normal text-zinc-100">
-                {variable.name}
-              </span>
-              {variable.note ? (
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <MessageSVG height="40" width="40" />
-                    </TooltipTrigger>
-                    <TooltipContent className="border-white/20 bg-white/10 text-white backdrop-blur-xl">
-                      <p>{variable.note}</p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              ) : null}
-            </div>
-            <div className="flex h-[6.5rem] w-[18.188rem] items-center justify-center gap-x-[3.125rem]">
-              <div className="flex h-[2.063rem] w-[13.563rem] items-center justify-center gap-x-3">
-                <div className="flex h-[2.063rem] w-[7.438rem] items-center justify-center text-base font-normal text-white text-opacity-50">
-                  {dayjs(variable.updatedAt).toNow(true)} ago by{' '}
-                </div>
-                <div className="flex h-[2.063rem] w-[5.375rem] items-center justify-center gap-x-[0.375rem]">
-                  <div className="flex h-[2.063rem] w-[3.5rem] items-center justify-center text-base font-medium text-white">
-                    {variable.lastUpdatedBy.name.split(' ')[0]}
-                  </div>
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage />
-                    <AvatarFallback>
-                      {variable.lastUpdatedBy.name.charAt(0).toUpperCase() +
-                        variable.lastUpdatedBy.name.slice(1, 2).toLowerCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                </div>
-              </div>
-              <ChevronDown
-                className={`h-[1.5rem] w-[1.5rem] text-zinc-400 transition-transform ${openSections.has(variable.id) ? 'rotate-180' : ''}`}
-              />
-            </div>
-          </CollapsibleTrigger>
-          <CollapsibleContent className="h-full w-full gap-y-24 rounded-b-lg bg-[#232424] p-4">
-            <Table className="h-full w-full">
-              <TableHeader className="h-[3.125rem] w-full">
-                <TableRow className="h-[3.125rem] w-full hover:bg-[#232424]">
-                  <TableHead className="h-full w-[10.25rem] border-2 border-white/30 text-base font-bold text-white">
-                    Environment
-                  </TableHead>
-                  <TableHead className="h-full border-2 border-white/30 text-base font-normal text-white">
-                    Value
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {values.map((env) => (
-                  <TableRow
-                    className="h-[3.125rem] w-full hover:cursor-pointer hover:bg-[#232424]"
-                    key={env.environment.id}
-                  >
-                    <TableCell className="h-full w-[10.25rem] border-2 border-white/30 text-base font-bold text-white">
-                      {env.environment.name}
-                    </TableCell>
-                    <TableCell className="h-full border-2 border-white/30 text-base font-normal text-white">
-                      {env.value}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CollapsibleContent>
-        </Collapsible>
-      </ContextMenuTrigger> */}
       <AccordionItem
         className="rounded-xl bg-white/5 px-5"
         key={variable.id}
@@ -176,7 +68,6 @@ export default function VariableCard(
         >
           <div className="flex gap-x-5">
             <div className="flex items-center gap-x-4">
-              {/* <SecretLogoSVG /> */}
               {variable.name}
             </div>
             {variable.note ? (
