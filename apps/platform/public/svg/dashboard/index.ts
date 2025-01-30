d5e0efb4a9113aa73abec77fc0a4b72e8d18db9e
kriptonian1 <shymal.sawan@gmail.com>
2025-01-30 17:15:47 +0530

    feat(platform): Add new access level SVGs and integrate into ProjectCard component

--- apps/platform/public/svg/dashboard/index.ts
+++ apps/platform/public/svg/dashboard/index.ts
@@ -2,5 +2,16 @@ import EnvironmentSVG from './environment.svg'
 import SecretSVG from './secret.svg'
 import FolderSVG from './folder.svg'
 import VariableSVG from './variable.svg'
+import PrivateSVG from './private.svg'
+import GlobalSVG from './global.svg'
+import InternalSVG from './internal.svg'
 
-export { EnvironmentSVG, SecretSVG, FolderSVG, VariableSVG }
+export {
+  EnvironmentSVG,
+  SecretSVG,
+  FolderSVG,
+  VariableSVG,
+  PrivateSVG,
+  GlobalSVG,
+  InternalSVG
+}
