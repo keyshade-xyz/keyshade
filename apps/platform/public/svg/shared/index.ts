9bffedc37f1b19546ef9848d3a3fa49a438a0a80
abracodeabra <akakiplimo@gmail.com>
2025-02-11 10:23:46 +0300

    feat(Roles): add roles tab in sidebar

--- apps/platform/public/svg/shared/index.ts
+++ apps/platform/public/svg/shared/index.ts
@@ -12,6 +12,7 @@ import VectorSVG from './vector.svg'
 import ErrorSVG from './Error.svg'
 import TrashSVG from './trash.svg'
 import CopySVG from './copy.svg'
+import RolesSVG from './roles.svg'
 
 export {
   DropdownSVG,
@@ -27,5 +28,6 @@ export {
   VectorSVG,
   ErrorSVG,
   TrashSVG,
-  CopySVG
+  CopySVG,
+  RolesSVG
 }
