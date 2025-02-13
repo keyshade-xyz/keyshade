655bd2631632fb0c567582997074bee193a4ef8b
abracodeabra <akakiplimo@gmail.com>
2025-02-13 10:48:46 +0300

    fix(platform): add error boundary to empty roles component

--- apps/platform/src/app/(main)/roles/page.tsx
+++ apps/platform/src/app/(main)/roles/page.tsx
@@ -1,9 +1,21 @@
 'use client'
 
-import React from 'react'
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary'
+import Error from 'next/error'
+import React, { Suspense } from 'react'
+
+function ErrorComponent() {
+  return <Error statusCode={404} />
+}
 
 function RolesPage(): React.JSX.Element {
-  return <></>
+  return (
+    <ErrorBoundary errorComponent={ErrorComponent}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>undefined roles</div>
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
 export default RolesPage
