cfcb42b141a6bc239f75adae3b990164abc3315f
rajdip-b <agentR47@gmail.com>
2025-03-07 23:54:28 +0530

    disabled lint for use-http hook

--- apps/platform/.eslintrc.cjs
+++ apps/platform/.eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-argument': 2,
     'no-nested-ternary': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off'
   }
