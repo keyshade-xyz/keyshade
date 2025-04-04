c8f7cc68573fe64b1e3d2032029e5da65d8c7b54
rajdip-b <agentR47@gmail.com>
2025-04-04 11:26:05 +0530

    feat(project): Fixed bugs with regenerating and storing private key

--- apps/api/src/project/project.service.ts
+++ apps/api/src/project/project.service.ts
@@ -283,6 +283,37 @@ export class ProjectService {
     // Check if project with this name already exists for the user
     dto.name && (await this.projectExists(dto.name, project.workspaceId))
 
+    // Check if the private key is to be stored but, either the project
+    // already stores the key, or the key to be stored is not specified
+    if (dto.storePrivateKey) {
+      if (project.storePrivateKey === true && project.privateKey) {
+        this.logger.log(
+          `Project ${project.slug} already stores the private key`
+        )
+        throw new BadRequestException(
+          constructErrorBody(
+            'Private key already stored',
+            'The project already stores the private key'
+          )
+        )
+      } else if (!dto.privateKey) {
+        this.logger.log(
+          `Private key to store not specified while updating project ${project.slug}`
+        )
+        throw new BadRequestException(
+          constructErrorBody(
+            'Private key missing',
+            'You did not specify the private key to store'
+          )
+        )
+      }
+    }
+
+    if (!dto.storePrivateKey && !dto.regenerateKeyPair) {
+      // Explicitly set dto.privateKey to null to avoid db issues
+      dto.privateKey = null
+    }
+
     if (dto.accessLevel) {
       this.logger.log(`Access level specified while updating project.`)
       const currentAccessLevel = project.accessLevel
@@ -361,7 +392,7 @@ export class ProjectService {
           await this.updateProjectKeyPair(
             project,
             dto.privateKey || project.privateKey,
-            dto.storePrivateKey
+            project.storePrivateKey || dto.storePrivateKey
           )
 
         privateKey = newPrivateKey
@@ -1394,8 +1425,8 @@ export class ProjectService {
         updatedVersions.push({
           id: version.id,
           value: await encrypt(
-            await decrypt(oldPrivateKey, version.value),
-            newPrivateKey
+            newPublicKey,
+            await decrypt(oldPrivateKey, version.value)
           )
         })
       }
