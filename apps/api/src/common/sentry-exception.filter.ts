17406902c394dd2616ebef7069132052f78d8653
rajdip-b <agentR47@gmail.com>
2025-04-04 13:56:51 +0530

    chore(api): Report only 500 errors

--- apps/api/src/common/sentry-exception.filter.ts
+++ apps/api/src/common/sentry-exception.filter.ts
@@ -1,4 +1,3 @@
-// src/filters/sentry-exception.filter.ts
 import { Catch, ArgumentsHost, HttpException } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
 import * as Sentry from '@sentry/node'
@@ -11,8 +10,7 @@ export class SentryExceptionFilter extends BaseExceptionFilter {
     const response = ctx.getResponse()
     const request = ctx.getRequest()
     try {
-      // Only proceed if we're in an HTTP context
-      if (request && response) {
+      if (request && response && exception.getStatus() === 500) {
         Sentry.withScope((scope) => {
           scope.addEventProcessor((event) => {
             event.request = {
@@ -27,22 +25,23 @@ export class SentryExceptionFilter extends BaseExceptionFilter {
           Sentry.captureException(exception)
         })
 
-        // Only call super if we have a valid HTTP context
-        // super.catch(exception, host)
+        response.status(500).json({
+          message: constructErrorBody(
+            'Internal Server Error',
+            'Something went wrong on our end. If the error persists, please get in touch with us at support@keyshade.xyz'
+          )
+        })
       } else {
-        // For non-HTTP contexts, just report to Sentry
-        Sentry.captureException(exception)
+        super.catch(exception, host)
       }
     } catch (filterError) {
-      // Ensure we don't crash the app if error handling fails
       Sentry.captureException(filterError)
       Sentry.captureException(exception)
     } finally {
-      response.status(500).json({
-        message: constructErrorBody(
-          'Internal Server Error',
-          'Something went wrong on our end. If the error persists, please get in touch with us at support@keyshade.xyz'
-        )
+      response.json({
+        message: exception['message'],
+        error: exception['name'],
+        statusCode: exception['status']
       })
     }
   }
