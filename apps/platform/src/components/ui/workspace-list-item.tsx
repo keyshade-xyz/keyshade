41d2266049b71af8e9784329cdf151e3937dc83a
Souvik Mukhopadhyay <146748505+Souvik9205@users.noreply.github.com>
2025-04-14 21:03:10 +0530

    feat(platform): Paginated List Component (#852)
    Co-authored-by: Sawan Bhattacharya <74916308+kriptonian1@users.noreply.github.com>
    Co-authored-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ apps/platform/src/components/ui/workspace-list-item.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { Check } from 'lucide-react'
+import { useAtom } from 'jotai'
+import { useRouter } from 'next/navigation'
+import type { WorkspaceWithTierLimitAndProjectCount } from '@keyshade/schema'
+import { cn } from '@/lib/utils'
+import { selectedWorkspaceAtom } from '@/store'
+import { CommandItem } from '@/components/ui/command'
+
+interface WorkspaceListItemProps {
+  workspace: WorkspaceWithTierLimitAndProjectCount
+  onClose: () => void
+}
+
+export function WorkspaceListItem({
+  workspace,
+  onClose
+}: WorkspaceListItemProps) {
+  const [selectedWorkspace, setSelectedWorkspace] = useAtom(
+    selectedWorkspaceAtom
+  )
+  const router = useRouter()
+
+  const isSelected = selectedWorkspace?.name === workspace.name
+
+  const handleSelect = () => {
+    setSelectedWorkspace(workspace)
+    router.refresh()
+    onClose()
+  }
+
+  return (
+    <CommandItem onSelect={handleSelect}>
+      <Check
+        className={cn('mr-2 h-4 w-4', isSelected ? 'opacity-100' : 'opacity-0')}
+      />
+      {workspace.icon ?? '🔥'} {workspace.name}
+    </CommandItem>
+  )
+}
