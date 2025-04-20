15759f7eaacbf96f59760f06134c041e91744baf
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2025-04-20 20:44:09 +0530

    refactor(platform): Update switch component styles for checked state (#870)
    Co-authored-by: Rajdip Bhattacharya <agentR47@gmail.com>

--- apps/platform/src/components/ui/switch.tsx
+++ apps/platform/src/components/ui/switch.tsx
@@ -8,7 +8,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-white/60 data-[state=unchecked]:bg-white/30 dark:focus-visible:ring-zinc-300 dark:focus-visible:ring-offset-zinc-950 dark:data-[state=checked]:bg-zinc-50 dark:data-[state=unchecked]:bg-zinc-800',
+      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-[#0DB2D4] data-[state=unchecked]:bg-white/30 dark:focus-visible:ring-zinc-300 dark:focus-visible:ring-offset-zinc-950 dark:data-[state=checked]:bg-[#0DB2D4] dark:data-[state=unchecked]:bg-zinc-800',
       className
     )}
     {...props}
