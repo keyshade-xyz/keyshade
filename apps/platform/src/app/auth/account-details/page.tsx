aee9b87fc24013c4a0045f63b3714cb9f20e216e
Sawan Bhattacharya <shymal.sawan@gmail.com>
2025-04-29 22:39:00 +0530

    fix(auth): change redirect method to use window.location.href after profile update

--- apps/platform/src/app/auth/account-details/page.tsx
+++ apps/platform/src/app/auth/account-details/page.tsx
@@ -63,7 +63,7 @@ export default function AuthDetailsPage(): React.JSX.Element {
         /**
          * redirect to dashboard after updating the profile details
          */
-        router.replace('/')
+        window.location.href='/'
       }
     } finally {
       toast.dismiss()
