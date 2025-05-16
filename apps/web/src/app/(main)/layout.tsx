3faf7af953c9de29e8c40e6a80f214057d0da25e
rajdip-b <agentR47@gmail.com>
2025-05-16 16:44:52 +0530

    fix(web): Broken alignment

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
-// import TopBanner from '@/components/topBanner'
 
 export default function MainLayout({
   children
@@ -9,7 +8,6 @@ export default function MainLayout({
 }): React.JSX.Element {
   return (
     <main lang="en">
-      {/* <TopBanner /> */}
       <div className="flex w-full justify-center">
         <Navbar />
       </div>
