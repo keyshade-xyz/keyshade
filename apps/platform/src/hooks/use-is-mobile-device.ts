156b3da4f6c27a3feb7716bebb864206fd2d16c6
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2025-06-14 16:45:37 +0530

    fix(platform): mobile detection logic and conditional rendering for overlay (#1001)
    Co-authored-by: Sawan Bhattacharya <shymal.sawan@gmail.com>
    Co-authored-by: Sawan Bhattacharya <74916308+kriptonian1@users.noreply.github.com>
    Co-authored-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ apps/platform/src/hooks/use-is-mobile-device.ts
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from 'react'
+
+/**
+ * Determine if the current device is a mobile device.
+ *
+ * @returns Returns true if the device is a mobile device, false otherwise.
+ */
+export function useIsMobileDevice(): boolean {
+  const [isMobile, setIsMobile] = useState(false)
+
+  const MOBILE_BREAKPOINT = 768
+
+  const checkMobile = useCallback(() => {
+    // Check if device supports touch
+    const isTouchDevice =
+      'ontouchstart' in window || navigator.maxTouchPoints > 0
+    const smallerDimension = Math.min(window.innerWidth, window.innerHeight)
+    const isSmallDevice = smallerDimension <= MOBILE_BREAKPOINT
+
+    const isMobileDevice = isTouchDevice && isSmallDevice
+
+    setIsMobile(isMobileDevice)
+  }, [])
+
+  useEffect(() => {
+    checkMobile()
+
+    window.addEventListener('resize', checkMobile)
+
+    screen.orientation.addEventListener('change', checkMobile)
+
+    return () => {
+      window.removeEventListener('resize', checkMobile)
+      screen.orientation.removeEventListener('change', checkMobile)
+    }
+  }, [checkMobile])
+
+  return isMobile
+}
