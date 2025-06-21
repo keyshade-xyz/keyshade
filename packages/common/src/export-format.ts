ae3e740e52ede15af8fa55c7260d3a0cb647ac4a
Giacomo Camerano <giacomo.camerano@gmail.com>
2025-06-21 11:21:33 +0200

    fix: stricter typechecking

--- packages/common/src/export-format.ts
+++ packages/common/src/export-format.ts
@@ -2,21 +2,22 @@ export enum ExportFormat {
   JSON = 'json'
 }
 
+export interface ExportFormatInfo {
+  label: string
+  mimeType: string
+  extension: string
+}
+
 /**
- * Metadata for each ExportFormat:
- *   - label: human-readable
- *   - mimeType: HTTP Content-Type
- *   - extension: file suffix
+ * Definitions for every ExportFormat:
+ *  • label:     human-readable
+ *  • mimeType:  HTTP Content-Type
+ *  • extension: file suffix
  */
-export const ExportFormatMetadata: Record<
-  ExportFormat,
-  { label: string; mimeType: string; extension: string }
-> = {
+export const EXPORT_FORMAT_INFO: Record<ExportFormat, ExportFormatInfo> = {
   [ExportFormat.JSON]: {
     label: 'JSON',
     mimeType: 'application/json',
     extension: 'json'
   }
 }
-
-export const ALL_EXPORT_FORMATS = Object.values(ExportFormat) as ExportFormat[]
