91f121e0bd46e648c5b2c29504e5d1cb97356920
rajdip-b <agentR47@gmail.com>
2025-06-23 13:28:16 +0530

    chore(cli): Add base integration command

---
+++ apps/cli/src/commands/integration.command.ts
@@ -0,0 +1,11 @@
+import BaseCommand from './base.command'
+
+export default class IntegrationCommand extends BaseCommand {
+  getName(): string {
+    return 'integration'
+  }
+
+  getDescription(): string {
+    return 'Manage your integrations in keyshade.'
+  }
+}
