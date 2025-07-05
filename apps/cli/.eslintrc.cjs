b5c97687061b90bccf076625d5581a0c296db12d
Ananta Podder <ananta8967940712@gmail.com>
2025-07-05 23:29:33 +0530

    removed commented codes

--- apps/cli/.eslintrc.cjs
+++ apps/cli/.eslintrc.cjs
@@ -29,17 +29,6 @@ module.exports = {
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
     'space-before-function-paren': 'off',
     '@typescript-eslint/member-delimiter-style': 'off',
-    // '@typescript-eslint/naming-convention': [
-    //   'error',
-    //   {
-    //     selector: 'variableLike',
-    //     format: null,
-    //     custom: {
-    //       regex: '^[a-zA-Z0-9_]+$',
-    //       match: true
-    //     }
-    //   }
-    // ]
     'id-match': ['error', '^[a-zA-Z0-9_]+$']
   }
 }
