afd0ebee606bf8d03a451f3a71465fcfc1216875
levanter914 <shamkashyap25@gmail.com>
2025-07-21 16:31:27 +0530

    fix: changed to using @common-js/is-ip

--- apps/api/src/auth/service/auth.service.ts
+++ apps/api/src/auth/service/auth.service.ts
@@ -19,7 +19,7 @@ import { createUser, getUserByEmailOrId } from '@/common/user'
 import { UserWithWorkspace } from '@/user/user.types'
 import { Response } from 'express'
 import SlugGenerator from '@/common/slug-generator.service'
-import { isIP } from 'is-ip'
+import * as isIpModule from '@common.js/is-ip'
 import { UAParser } from 'ua-parser-js'
 import { toSHA256 } from '@/common/cryptography'
 
@@ -62,6 +62,7 @@ export class AuthService {
       ''
     ).trim()
 
+    const isIP = (isIpModule as any).default
     if (!isIP(rawIp)) {
       throw new Error(`Invalid IP address: ${rawIp}`)
     }
