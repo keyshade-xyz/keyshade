7f43c6e78b177df33db052e22d81a54f0506d78f
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-08-01 11:09:49 -0700

    add the getOptions attribute to the create.integration implementation file

--- apps/cli/src/commands/integration/create.integration.ts
+++ apps/cli/src/commands/integration/create.integration.ts
@@ -1,4 +1,5 @@
 import BaseCommand from '../base.command'
+import type { CommandOption } from '@/types/command/command.types'
 
 export class CreateIntegration extends BaseCommand {
   getName(): string {
@@ -7,6 +8,44 @@ export class CreateIntegration extends BaseCommand {
 
   getDescription(): string {
     return 'Create an integration'
+  }
+
+  getOptions(): CommandOption[] {
+    return [
+      {
+        short: '-p',
+        long: '--project <project Slug>',
+        description: 'Slug of the project which the integration will listen to.'
+      },
+      {
+        short: '-e',
+        long: '--environments <env slugs>',
+        description:
+          'Comma separated list of environments the integration will listen for.'
+      },
+      {
+        short: '-n',
+        long: '--notify-on <LIST>',
+        description: 'Array of events that the integration listens to. Optional'
+      },
+      {
+        short: '-m',
+        long: '--metadata <JSON OBJECT>',
+        description:
+          'Metadata specific to the environment. Expects a JSON object. Takes precedence to --metadata-file'
+      },
+      {
+        short: '-mf',
+        long: '--metadata-file <path/to/file>',
+        description:
+          'Same as --metadata, just expects a file that will contain the details in JSON object'
+      },
+      {
+        short: '-t',
+        long: '--type <>',
+        description: 'The type of notification'
+      }
+    ]
   }
 
   canMakeHttpRequests(): boolean {
