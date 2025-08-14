63785198b30e45e54ddf440622b1b9a0bc7be90b
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-08-13 20:32:17 -0700

    add the integration.delete.ts methods

--- apps/cli/src/commands/integration/delete.integration.ts
+++ apps/cli/src/commands/integration/delete.integration.ts
@@ -1,5 +1,10 @@
 import BaseCommand from '../base.command'
-import type { CommandOption } from '@/types/command/command.types'
+import ControllerInstance from '@/util/controller-instance'
+import {
+  type CommandOption,
+  type CommandActionData
+} from 'src/types/command/command.types'
+import { Logger } from '@/util/logger'
 
 export class DeleteIntegration extends BaseCommand {
   getName(): string {
@@ -22,5 +27,28 @@ export class DeleteIntegration extends BaseCommand {
 
   canMakeHttpRequests(): boolean {
     return true
+  }
+
+  async action({ args }: CommandActionData): Promise<void> {
+    const [integrationSlug] = args
+
+    if (!integrationSlug) {
+      Logger.error('Integration Slug is required')
+      return
+    }
+
+    Logger.info('Deleting Integration...')
+
+    const { success, error } =
+      await ControllerInstance.getInstance().integrationController.deleteIntegration(
+        { integrationSlug },
+        this.headers
+      )
+
+    if (success) {
+      Logger.info('Integration deleted successfully')
+    } else {
+      this.logError(error)
+    }
   }
 }
