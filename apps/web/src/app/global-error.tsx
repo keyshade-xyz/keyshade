f758b28703783ee542d708e4f80f714e81085e66
Sawan Bhattacharya <shymal.sawan@gmail.com>
2025-10-13 18:17:45 +0530

    fix: eslint error

--- apps/web/src/app/global-error.tsx
+++ apps/web/src/app/global-error.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as Sentry from '@sentry/nextjs'
-import NextError from 'next/error'
+import Error from 'next/error'
 import React, { useEffect } from 'react'
 
 export default function GlobalError({
@@ -20,7 +20,7 @@ export default function GlobalError({
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
         generic error message. */}
-        <NextError statusCode={0} />
+        <Error statusCode={0} />
       </body>
     </html>
   )
