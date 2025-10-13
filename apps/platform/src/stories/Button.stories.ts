01cc5a666f4a664d9ac1f797ace0ef7e2b32ed40
Sawan Bhattacharya <shymal.sawan@gmail.com>
2025-10-14 04:31:22 +0530

    feat: add color token

--- apps/platform/src/stories/Button.stories.ts
+++ apps/platform/src/stories/Button.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite'
+import type { Meta, StoryObj } from '@storybook/nextjs'
 import { fn } from 'storybook/test'
 import { Button } from './button-example'
 
