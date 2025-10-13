411b85297aedaf65f7c3f4c25bbe3797eef3a908
rajdip-b <agentR47@gmail.com>
2025-10-13 19:55:02 +0530

    update lint issue

--- apps/web/src/app/global-error.tsx
+++ apps/web/src/app/global-error.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import * as Sentry from '@sentry/nextjs'
-import Error from 'next/error'
-import React, { useEffect } from 'react'
+import NextError from 'next/error'
+import { useEffect } from 'react'
 
 export default function GlobalError({
   error
 }: {
   error: Error & { digest?: string }
-}): React.JSX.Element {
+}) {
   useEffect(() => {
     Sentry.captureException(error)
   }, [error])
@@ -20,7 +20,7 @@ export default function GlobalError({
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
         generic error message. */}
-        <Error statusCode={0} />
+        <NextError statusCode={0} />
       </body>
     </html>
   )
