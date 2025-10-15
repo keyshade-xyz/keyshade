c5bf6f4b5a16b6cee37364a2127841463263d708
Sawan Bhattacharya <shymal.sawan@gmail.com>
2025-10-15 21:15:29 +0530

    feat: updated button, and badge

--- apps/platform/src/components/ui/button.tsx
+++ apps/platform/src/components/ui/button.tsx
@@ -4,31 +4,30 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:bg-white/15 disabled:cursor-not-allowed disabled:opacity-50 disabled:text-white/56 disabled:border disabled:border-[#FAFAFA]/10 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300',
+  'inline-flex items-center justify-center whitespace-nowrap cursor-pointer rounded-md text-sm font-normal ring-offset-blue-500 transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 disabled:text-white/56 disabled:border disabled:border-[#FAFAFA]/10',
   {
     variants: {
       variant: {
-        default:
-          'bg-[#26282C] border border-[#FAFAFA]/10 shadow-inherit shadow-black/50 text-white/56 hover:bg-[#17191B]/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90',
-        destructive:
-          'bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90',
-        outline:
-          'border border-white/10 text-white/60 hover:bg-white/10 hover:text-white dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50',
+        primary:
+          'bg-primary-700 border border-white/35 shadow-inherit text-white hover:bg-primary-800 shadow-[inset_0_0_3px_1px_oklch(1_0_0_/_25%)]',
         secondary:
-          'bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80',
+          'bg-primary-1000 text-primary-400 border border-primary-400 hover:bg-primary-1100 hover:border-primary-200',
+        outline:
+          'border border-white/8 text-white hover:border-white/4 hover:text-white/80',
+        danger: 'bg-red-600 text-white hover:bg-red-700',
         ghost:
-          'hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 disabled:bg-transparent',
-        link: 'text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50'
+          'hover:text-white/70 disabled:bg-transparent disabled:border-transparent',
+        link: 'text-primary-300 underline-offset-4 hover:underline disabled:text-primary-300/56 disabled:border-transparent'
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-3 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10'
       }
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default'
     }
   }
