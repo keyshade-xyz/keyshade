2aa1766f800ebd579ebebd0ff465edf1affed451
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-10-31 02:21:02 -0400

    fix: improve homepage layout on small mobile screens

--- apps/web/src/components/hero/index.tsx
+++ apps/web/src/components/hero/index.tsx
@@ -12,12 +12,12 @@ function Hero(): React.JSX.Element {
       <div className="relative -top-8 flex min-h-screen w-full flex-col items-center justify-center  bg-[url(/hero/colour-bg.svg)]  bg-cover bg-no-repeat pb-4 md:pt-[10rem]">
         <section className="flex flex-col items-center gap-8 py-[6.88rem] ">
           <h1
-            className={` text-brandBlue w-[25rem] text-center text-4xl md:w-auto md:text-7xl`}
+            className={` text-brandBlue w-full text-center text-4xl md:w-auto md:text-7xl`}
             style={{ textShadow: '0px 4px 4px rgba(202, 236, 241, 0.25)' }}
           >
             The better .env file replacement
           </h1>
-          <span className="text-brandBlue/80 flex w-[20rem] flex-col justify-center text-center text-sm md:w-[35rem] md:text-xl md:leading-[2rem]">
+          <span className="text-brandBlue/80 flex w-full max-w-md flex-col justify-center text-center text-sm md:w-[35rem] md:text-xl md:leading-[2rem]">
             <p>Stop DMing your Environment variables</p>
             <p>And start syncing them securely & instantly </p>
           </span>
