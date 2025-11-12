0b0720d4279b6c841079c8efc2a1cd549b8c53d7
rajdip-b <agentR47@gmail.com>
2025-11-12 11:27:14 +0530

    chore(cli): update package managers to use v3.6.4 [skip ci]

--- Formula/keyshade.rb
+++ Formula/keyshade.rb
@@ -1,18 +1,18 @@
 class Keyshade < Formula
   desc "Keyshade CLI - Secure, real-time secret and configuration management"
   homepage "https://keyshade.io"
-  version "3.6.3"
+  version "3.6.4"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/keyshade-xyz/keyshade/releases/download/v2.43.0/keyshade-cli-macos-x64"
-      sha256 "c243d8963155e7590a1533e7b28e4eac83bac30cb04a0f6a7fc789c6c6fe0939"
+      url "https://github.com/keyshade-xyz/keyshade/releases/download/v2.43.1/keyshade-cli-macos-x64"
+      sha256 "f051b3abec538503ff937cd40b79c5aa33a7b5d20f35956b4fd8da82f701202f"
     end
 
     on_arm do
-      url "https://github.com/keyshade-xyz/keyshade/releases/download/v2.43.0/keyshade-cli-macos-arm64"
-      sha256 "08617aa6aad15d9fab44938cb9656cce7c47f0dfbad7de1dc3e4261a9d0cd652"
+      url "https://github.com/keyshade-xyz/keyshade/releases/download/v2.43.1/keyshade-cli-macos-arm64"
+      sha256 "f0c4fa6d626281e753e560941125a62d64e3c6c2ab0c0f8542186321a13cf24c"
     end
   end
 
