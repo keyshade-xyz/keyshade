4a90094a25febf72a63107694a6d01e5687add38
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2025-11-22 00:42:25 +0530

    feat(api): updated prisma version to 7, added config file & updated the script paths (#1253)
    Co-authored-by: rajdip-b <agentR47@gmail.com>

---
+++ apps/api/src/prisma/prisma.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  schema: './schema.prisma',
+  migrations: {
+    path: './migrations'
+  },
+  datasource: {
+    url: env('DATABASE_URL')
+  }
+})
