8e4ef3dd1cc40f23d01428d7764a19c53df68c69
ILOVEZERI333 <ilovezeri333@gmail.com>
2026-02-20 12:49:26 -0800

    fix: check length to show error message before regex catches it

--- packages/schema/src/alphanumeric/index.ts
+++ packages/schema/src/alphanumeric/index.ts
@@ -2,25 +2,25 @@ import { z } from 'zod'
 
 export const AlphaNumericStringSchema = z
   .string()
-  .regex(/^[a-zA-Z0-9]+$/)
   .refine((val) => val.length > 0, {
     message: 'Alphanumeric string cannot be empty'
   })
+  .regex(/^[a-zA-Z0-9]+$/)
 
 // This is a string that can only contain alphanumeric characters and underscores
 export const VariableAlphaNumericStringSchema = z
   .string()
-  .regex(/^[a-zA-Z0-9_]+$/)
   .refine((val) => val.length > 0, {
     message: 'Variable alphanumeric string cannot be empty'
   })
+  .regex(/^[a-zA-Z0-9_]+$/)
 
 export const ExtendedAlphaNumericStringSchema = z
   .string()
-  .regex(/^[a-zA-Z0-9_-]+$/)
   .refine((val) => val.length > 0, {
     message: 'Extended alphanumeric string cannot be empty'
   })
+  .regex(/^[a-zA-Z0-9_-]+$/)
 
 export const ColorCodeAlphaNumericStringSchema = z
   .string()
