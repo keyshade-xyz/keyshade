849acb859b207e06dbeb7417f01b765037408f0e
josue <jcruz323@mail.ccsf.edu>
2026-02-21 12:12:03 -0800

    refactor: enforce alphanumeric OTP validation in otp-input-form.tsx making reference to alphanumeric schema package

--- apps/platform/src/components/auth/otp/otp-input-form.tsx
+++ apps/platform/src/components/auth/otp/otp-input-form.tsx
@@ -1,13 +1,13 @@
 import { REGEXP_ONLY_DIGITS_AND_CHARS } from 'input-otp'
 import type { Dispatch, SetStateAction } from 'react'
 import React, { useState } from 'react'
-import { z } from 'zod'
 import { toast } from 'sonner'
 import { useAtom } from 'jotai'
 import { useRouter } from 'next/navigation'
 import { posthog } from 'posthog-js'
 import Cookies from 'js-cookie'
 import { LoadingSVG } from '@public/svg/shared'
+import { OTPAlphaNumericStringSchema } from '@keyshade/schema/src'
 import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/components/ui/input-otp'
 import { Button } from '@/components/ui/button'
 import { useHttp } from '@/hooks/use-http'
@@ -51,7 +51,7 @@ export default function OtpInputForm({
 
     const isOtpValid = isAlphanumeric(otp)
 
-    if (!isEmailValid(user.email) || !isOtpValid) {
+    if (!user?.email || !isEmailValid(user.email as string) || !isOtpValid) {
       toast.error('Invalid OTP', {
         description: (
           <p className="text-xs text-red-300">
@@ -92,15 +92,7 @@ export default function OtpInputForm({
   }
 
   const isAlphanumeric = (value: string): boolean => {
-    const result = z
-      .string()
-      .length(6)
-      .refine((str) => /^[a-z0-9]+$/i.test(str), {
-        message: 'OTP must be alphanumeric'
-      })
-      .safeParse(value)
-
-    return result.success
+    return OTPAlphaNumericStringSchema.safeParse(value).success
   }
 
   const isButtonDisabled = (): boolean => {
@@ -108,7 +100,7 @@ export default function OtpInputForm({
       isLoading ||
       otp.length !== 6 ||
       !isAlphanumeric(otp) ||
-      !isEmailValid(user?.email)
+      !user?.email || !isEmailValid(user.email as string)
     )
   }
 
